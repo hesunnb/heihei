253f211004609404dd9cff52f0d7add2e7fe79c7
hesunnb <hesun63@gmail.com>
2016-02-14 11:10:42 -0500

    Update Reverse Integer.java

--- Reverse Integer.java
+++ Reverse Integer.java
@@ -6,7 +6,24 @@ public class Solution {
     public int reverseInteger(int n) {
         // Write your code here
         
-		String result = "";
+        int rst = 0;
+        
+        while(n != 0) 
+        {
+            int next_rst = rst * 10 + n % 10; //把尾数提取出来，存下
+            n = n / 10; //把尾数撇掉
+            if(next_rst/10 != rst) //这句就是看看有没有越界发生，发生啦截取后的值除以10以后肯定和原来的不一样
+            //int的值在计算的情况下，结果大于intmax会截取比如int n = 123456789 * 123456789，会截取；但是如果直接int n = 123456789999L, 就是把long给int，是不可以哒
+            {
+                rst  = 0;
+                break;
+            }
+            rst = next_rst; //没有越界的情况下给结果
+        }
+        return rst;
+        
+        
+		/*String result = "";
 		String temp = String.valueOf(n);
 		Stack<Character> st = new Stack<Character>();
 		
@@ -33,6 +50,6 @@ public class Solution {
 		else
 		{
 		    return Integer.parseInt(result);
-		}
+		}*/
     }
 }
