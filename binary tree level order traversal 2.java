87cf1c7f3be429a3887467d15044b7a63ab5d544
hesunnb <hesun63@gmail.com>
2016-03-08 18:04:41 -0500

    Update and rename binary tree level order traversal 2 to binary tree level order traversal 2.java

--- binary tree level order traversal 2.java
+++ binary tree level order traversal 2.java
@@ -16,6 +16,8 @@ public class Solution {
      * @param root: The root of binary tree.
      * @return: buttom-up level order a list of lists of integer
      */
+     
+    //BFS正常遍历
     public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) {
         // write your code here
          ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
@@ -48,5 +50,28 @@ public class Solution {
                 result.add(0, level); //就和１那个问题差了这句话，这个就是每次在result的前面加level，然后就是倒序了
             }
             return result;
+    }
+    
+    //DFS遍历
+    public ArrayList<ArrayList<Integer>> levelOrderBottom(TreeNode root) 
+    {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        levelmaker(result, root, 0); //第一行是0level, 依次类推
+        return result;
+    }   
+    
+    private void levelmaker(ArrayList<ArrayList<Integer>> result, TreeNode root, int level)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        if(level >= result.size()) //实际上只是每次等于起作用, 保险起见用成>=
+        {
+            result.add(0, new ArrayList<Integer>()); //leve>=list.size()的时候就是第一次进入这个层的时候, 每次第一回到一个层都要创建一个盒
+        }
+        levelmaker(result, root.left, level + 1); //对左边levelmaker
+        levelmaker(result, root.right, level + 1); //对右边levelmaker
+        result.get(result.size() - level - 1).add(root.val); //左右都建完啦, 把自己的值加到相应的盒里
     }
 }
