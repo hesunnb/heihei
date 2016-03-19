9a885195eeabaeee9a9bec851d7b1709fa9d2bfa
hesunnb <hesun63@gmail.com>
2016-03-19 14:54:35 -0400

    Update Count 1 in Binary.java

--- Count 1 in Binary.java
+++ Count 1 in Binary.java
@@ -3,6 +3,9 @@ public class Solution {
      * @param num: an integer
      * @return: an integer, the number of ones in num
      */
+     
+    
+    //version 1:
     public int countOnes(int num) {
         // write your code here
         
@@ -18,5 +21,20 @@ public class Solution {
             num = num >>> 1; //逻辑右移，正负数高位都补0，所以就按位与统计1的个数就行啦
         }
         return result;
+    }
+    
+    
+    
+    
+    
+    
+    //version 2:
+    public int countOnes(int num) {
+        int count = 0;
+        while (num != 0) {
+            num = num & (num - 1); //与自己减1按位与, 会使每次num中1的个数少一个
+            count++;
+        }
+        return count;
     }
 };
