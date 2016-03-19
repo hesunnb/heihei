b8d230f0fdc17be74fd4486800d508660676ff6e
hesunnb <hesun63@gmail.com>
2016-03-19 14:36:43 -0400

    Update Add Binary.java

--- Add Binary.java
+++ Add Binary.java
@@ -8,7 +8,82 @@ public class Solution {
     //思路就是模拟二进制的加法运算，求余得结果，求商得进位
     public String addBinary(String a, String b) {
         // Write your code here
-        if(a.length() < b.length()) //这里确保a最大，做和，谁最大都行
+        
+        
+        
+        //version 1: 1个while
+        if(a == null || b == null)
+        {
+            return null;
+        }
+        
+        int pa = a.length() - 1;
+        int pb = b.length() - 1;
+        StringBuilder sb = new StringBuilder();
+        int carries = 0;
+        while(pa >= 0 || pb >= 0 || carries == 1) //把最后的carries是不是1的判断也合并到这里来, 就只剩一个while啦
+        {
+            int sum = carries;
+            if(pa >= 0)
+            {
+                sum += a.charAt(pa--) - '0';
+            }
+            if(pb >= 0)
+            {
+                sum += b.charAt(pb--) - '0';
+            }
+            sb.insert(0, sum % 2); //这里用insert在头部插入就行
+            carries = sum / 2;   
+        }
+        
+        return sb.toString();
+        //如果要是10进制以内, 就加入一个k代表进制, 然后每次sum % k, sum / k就行啦
+        
+        
+        
+        
+        
+        
+        //version 2: 1个while还有一个if
+        if(a == null || b == null)
+        {
+            return null;
+        }
+        
+        int pa = a.length() - 1;
+        int pb = b.length() - 1;
+        StringBuilder sb = new StringBuilder();
+        int carries = 0;
+        while(pa >= 0 || pb >= 0) //把最后的carries是不是1的判断也合并到这里来, 就只剩一个while啦
+        {
+            int sum = carries;
+            if(pa >= 0)
+            {
+                sum += a.charAt(pa--) - '0';
+            }
+            if(pb >= 0)
+            {
+                sum += b.charAt(pb--) - '0';
+            }
+            sb.insert(0, sum % 2); //这里用insert在头部插入就行
+            carries = sum / 2;   
+        }
+        
+        if(carries != 0)
+        {
+            sb.insert(0, carries);
+        }
+        
+        return sb.toString();
+        
+        
+        
+        
+        
+        
+        
+        //version 3: 2个while
+        /*if(a.length() < b.length()) //这里确保a最大，做和，谁最大都行
         {
             String temp = a;
             a = b;
@@ -42,6 +117,6 @@ public class Solution {
         {
             result = "1" + result;
         }
-        return result;
+        return result;*/
     }
 }
