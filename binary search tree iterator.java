27bd973e2617d88b9ef528ed7cde00730c740199
hesunnb <hesun63@gmail.com>
2016-03-19 12:20:28 -0400

    Update and rename binary search tree iterator to binary search tree iterator.java

--- binary search tree iterator.java
+++ binary search tree iterator.java
@@ -18,7 +18,9 @@
 public class BSTIterator {
     
     //等价于写非递归的中序遍历
-    //空间复杂度是O(h),时间复杂度都是O(1)
+    
+    //Extra memory usage O(h), h is the height of the tree: 比如这棵树全是左子树, 那么一次向stack中加入的就是这个树的高
+    //度, 其余的时候stack都会有弹出, 用的都比O(h)要小
     
     //@param root: The root of binary tree.
     private TreeNode curt = null;
@@ -37,7 +39,8 @@ public class BSTIterator {
     //@return: return next node
     public TreeNode next() {
         // write your code here
-        while(curt != null)
+        while(curt != null) //这个地方不能加!st.isEmpty(), 不能完全复制上面的条件, 因为有cur为空而stack不为空的情况, 
+        //要是用上面的条件, 就会null = null.left啦
         {
             st.push(curt); //从根开始一连串把根左都加入栈
             curt = curt.left;
