5f56a2a6a9ad01d82882ebe34c817477852a2020
hesunnb <hesun63@gmail.com>
2016-07-16 17:12:47 -0400

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -49,3 +49,25 @@ public class Solution {
         return Math.max(leftSub + rightSub, Math.max(left, right));
     }
 }
+
+
+
+//Minimum Subarray:
+public int minSubArray(ArrayList<Integer> nums) {
+        // write your code
+        
+        if(nums == null || nums.size() == 0) {
+            return 0;
+        }
+        
+        int minSoFar = nums.get(0);
+        int minEndHere = nums.get(0);
+        
+        for(int i = 1; i < nums.size(); i++) {
+            
+            minEndHere = Math.min(minEndHere + nums.get(i), nums.get(i));
+            minSoFar = Math.min(minSoFar, minEndHere);
+        }
+        
+        return minSoFar;
+    }
