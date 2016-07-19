d06427e2b9deeb346094cf50343e53e20d2e387d
hesunnb <hesun63@gmail.com>
2016-07-19 14:35:00 -0400

    Update remove duplicates from sorted array.java

--- remove duplicates from sorted array.java
+++ remove duplicates from sorted array.java
@@ -4,8 +4,41 @@ public class Solution {
      * @return : return an integer
      */
      
+    //version 1: (首选)
+    public int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+        	return 0;
+        }
+        int result = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+        	if (nums[i] == nums[i + 1]) {
+        		continue;
+        	} else {
+        		nums[result] = nums[i + 1];
+        		result++;
+        	}
+        }
+        return result;
+    }
     
-    //version 1: (首选)就是扫一遍, 两个指针(i和k)
+
+    //version 2: 和上面同样的思路, 不同的写法
+    public int removeDuplicates(int[] A) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        
+        int size = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != A[size]) {
+                A[++size] = A[i];
+            }
+        }
+        return size + 1;
+    }
+    
+    
+    //version 3: 就是扫一遍, 两个指针(i和k)
     public int removeDuplicates(int[] nums) {
         // write your code here
         if(nums == null || nums.length == 0)
@@ -29,42 +62,4 @@ public class Solution {
     
     
     
-    
-    //version 2: 和上面同样的思路, 不同的写法
-    public int removeDuplicates(int[] A) {
-        if (A == null || A.length == 0) {
-            return 0;
-        }
-        
-        int size = 0;
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] != A[size]) {
-                A[++size] = A[i];
-            }
-        }
-        return size + 1;
-    }
-    
-    
-    
-    
-    
-    
-    
-    //version 3: 也是一样
-    public int removeDuplicates(int[] nums) {
-        if (nums == null || nums.length == 0) {
-        	return 0;
-        }
-        int result = 1;
-        for (int i = 0; i < nums.length - 1; i++) {
-        	if (nums[i] == nums[i + 1]) {
-        		continue;
-        	} else {
-        		nums[result] = nums[i + 1];
-        		result++;
-        	}
-        }
-        return result;
-    }
 }
