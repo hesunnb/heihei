ce1e0e5b9a42c47378cf49dcaedd070f1b135ada
hesunnb <hesun63@gmail.com>
2016-07-19 01:06:17 -0400

    Update search insert postition.java

--- search insert postition.java
+++ search insert postition.java
@@ -1,3 +1,15 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0 
+*/
+
 public class Solution {
     /** 
      * param A : an integer sorted array
@@ -17,14 +29,13 @@ public class Solution {
         while(start + 1 < end)
         {
            int mid = start + (end - start) / 2;
-           if(A[mid] < target)
-           {
-               start = mid;
-           }
-           else
-           {
-               end = mid;
-           }
+            if(nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
         }
         
         //退出来以后，start和end的值现在已经固定了，接下来就是判断target相对于start和end的位置
