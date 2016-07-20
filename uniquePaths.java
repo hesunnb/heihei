37fb03832bbbc6f61bac5291876bb30b2fdfc2ec
hesunnb <hesun63@gmail.com>
2016-07-20 09:58:16 -0400

    Update and rename unique paths to uniquePaths.java

--- uniquePaths.java
+++ uniquePaths.java
@@ -9,6 +9,10 @@ public class Solution {
             return 0;
         }
         
+        if(m == 1 || n == 1) {
+            return 1;
+        }
+        
         int sum[][] = new int[m][n];
         sum[0][0] = 1;
         
@@ -31,5 +35,29 @@ public class Solution {
             }
         }
         return sum[m - 1][n - 1];
+    }
+   
+   
+    // Space Optimization
+    public int uniquePaths(int m, int n) {
+        
+        if(m == 0 || n == 0) {
+            return 0;
+        }
+        if(m == 1 || n == 1) {
+            return 1;
+        }
+        
+        int result[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            result[i] = 1;
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                result[j] += result[j - 1];
+            }
+        }
+        return result[n - 1];
     }
 }
