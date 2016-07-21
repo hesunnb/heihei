d14a6c0ea764e6b6896d1e32828197d6beeb9472
hesunnb <hesun63@gmail.com>
2016-07-21 10:50:37 -0400

    Update SubarraySum.java

--- SubarraySum.java
+++ SubarraySum.java
@@ -15,7 +15,7 @@ public class Solution {
         Map<Integer, Integer> m = new HashMap<Integer, Integer>();
         
         int sum = 0;
-        m.put(0, -1); //在-1的位置放置0, 对于和为0的子数组开头的情况(1, -1 ...)
+        m.put(0, -1); //在-1的位置放置0, -1位置的前n项和为0, 对于和为0的子数组开头的情况(1, -1 ...)
         for(int i = 0; i < nums.length; i++) { //从头开始求和, 如果再后面遇到和与之前一样的, 说明中间夹了一个和为0的子数组, 返回这个子数组的开头和结尾坐标即可
             sum += nums[i];
             if(m.containsKey(sum)) {
