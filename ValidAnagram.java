1e2501f1fd99e578535b18aebd514e274649fa49
hesunnb <hesun63@gmail.com>
2016-07-21 17:46:53 -0400

    Create ValidAnagram.java

---
+++ ValidAnagram.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /**
+     * @param s: The first string
+     * @param b: The second string
+     * @return true or false
+     */
+    public boolean anagram(String s, String t) {
+        // write your code here
+        
+        if(s.length() != t.length()) {
+            return false;
+        }
+        
+        int[] result = new int[256];
+        for(int i = 0; i < s.length(); i++) {
+            result[(int)s.charAt(i)]++;
+        }
+        
+        for(int i = 0; i < t.length(); i++) {
+            result[(int)t.charAt(i)]--;
+            if(result[(int)t.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
