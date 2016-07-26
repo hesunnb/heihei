ce54368e7f41baa4da710273537aa0ee174e1323
hesunnb <hesun63@gmail.com>
2016-07-26 13:05:15 -0400

    Update and rename space replacement.java to spaceReplacement.java

--- spaceReplacement.java
+++ spaceReplacement.java
@@ -1,3 +1,19 @@
+/*
+Write a method to replace all spaces in a string with %20. The string is given in a characters array, 
+you can assume it has enough space for replacement and you are given the true length of the string.
+
+You code should also return the new length of the string after replacement.
+
+Notice
+If you are using Java or Python，please use characters array instead of string.
+
+Example
+
+Given "Mr John Smith", length = 13.
+
+The string after replacement should be "Mr%20John%20Smith", you need to change the string in-place and return the new length 17.
+*/
+
 public class Solution {
     /**
      * @param string: An array of Char
