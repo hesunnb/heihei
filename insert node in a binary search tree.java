a76c925b82a54c1143d25b5019229b47ea8764fe
hesunnb <hesun63@gmail.com>
2016-08-02 14:02:28 -0400

    Update insert node in a binary search tree.java

--- insert node in a binary search tree.java
+++ insert node in a binary search tree.java
@@ -17,43 +17,6 @@ public class Solution {
      */
     
     //递归非递归都是O(logn)
-    //非递归
-    public TreeNode insertNode(TreeNode root, TreeNode node)
-    {
-       if(root == null)
-       {
-           return node; //node本身应该不能是空
-       }
-       TreeNode temp = root;
-       TreeNode last = null;
-       
-       while(temp != null)
-       {
-            last = temp; //last保留上一个节点
-            if(temp.val > node.val) //temp负责向下查找，找到temp就为空啦，退出循环
-            {
-                temp = temp.left;
-            }
-            else if(temp.val < node.val) //如果不加else这里就会连续判断了，temp的值刚改变就参与到下一个判断了
-            {
-                temp = temp.right;
-            }
-       }
-       if(last != null) //从last点开始把node加进去,last肯定不是空，所以这个判断可以没有
-       {
-           if(last.val > node.val)
-           {
-               last.left = node;
-           }
-           else if(last.val < node.val)
-           {
-               last.right = node;
-           }
-       }
-       return root;
-    }
-     
-     
     //递归:
     public TreeNode insertNode(TreeNode root, TreeNode node) {
         // write your code here
@@ -68,5 +31,32 @@ public class Solution {
             root.left = insertNode(root.left, node); //右节点作为根传进去
         }
         return root; //返回root, 整体并不需要遍历整棵树
+    }
+    
+    
+    //非递归
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+        // write your code here
+        
+        if(root == null) {
+            return node; //如果根为空，要插入的节点便直接成了根；其余情况node直接作为左右节点返回了,node本身不能是空
+        }
+        
+        TreeNode temp = root;
+        TreeNode last = temp;
+        while(temp != null) {
+            last = temp; //last保留上一个节点
+            if(temp.val < node.val) { //temp负责向下查找，找到temp就为空啦，退出循环
+                temp = temp.right;
+            } else { //如果不加else这里就会连续判断了，temp的值刚改变就参与到下一个判断了
+                temp = temp.left;
+            }
+        }
+        if(last.val < node.val) {
+            last.right = node;
+        } else {
+            last.left = node;
+        }
+        return root;
     }
 }
