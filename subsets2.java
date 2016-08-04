138bccf189d9d5e0ab622a96c6c801ba83f824be
hesunnb <hesun63@gmail.com>
2016-08-04 17:18:23 -0400

    Update and rename subsets2 to subsets2.java

--- subsets2.java
+++ subsets2.java
@@ -1,3 +1,24 @@
+/*Given a list of numbers that may has duplicate numbers, return all possible subsets
+Notice
+
+    Each element in a subset must be in non-descending order.
+    The ordering between two subsets is free.
+    The solution set must not contain duplicate subsets.
+    
+Example
+
+If S = [1,2,2], a solution is:
+
+[
+  [2],
+  [1],
+  [1,2,2],
+  [2,2],
+  [1,2],
+  []
+]
+*/
+
 class Solution {
     /**
      * @param S: A set of numbers.
@@ -33,6 +54,12 @@ class Solution {
         {
             if(i != pos && nums[i] == nums[i-1]) //比一问就多了这两行，就是递归回朔的时候如果后面的元素与前面的元素
             //相等，就不要后面的元素了，退回到循环处，而要第一个重复的元素【2,2】，要第一个2
+            
+            /*具体解释: i != pos所起到作用的地方是第一遍依次加入数的时候, [1,2]与[1,2,2],
+            最后一个2是i = pos, 所以可以正常加入; 以[1,2,2,3]为例, 返回remove的时候, remove到[1,2]这个时候加入3
+            [1,2,3]加入result, 这是针对两个2中第一个2, 当继续remove就会回到[1], 然后它要加入第二个2, 此时i != pos &&
+            nums[i] == nums[i-1], 所以不加入第二个2, 因为加入了第二个2, 进行的也是和第一个2重复的工作, 舍掉了*/
+            
             {
                 continue;
             }
