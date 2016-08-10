bd7cc4235285d6a80c806c9936eff9b38bd17857
hesunnb <hesun63@gmail.com>
2016-08-10 15:36:10 -0400

    Update UglyNumberII.java

--- UglyNumberII.java
+++ UglyNumberII.java
@@ -54,3 +54,57 @@ class Solution {
 在multi2的地方每次都成以2, 在multi3的地方每次都乘以3, 在multi5的地方每次都乘以5
 每次next2, next3, next5所在的下标的值如果最小成为了丑数, 就把下标+1, 用下一个丑数接着运算
 */
+
+
+//这些是向super ugly number过度的几个版, 就是把值都存到数组里操作
+public int nthUglyNumber(int n) {
+        int[] res = new int[n];
+        res[0] = 1;
+        int[] next = new int[3];
+        int[] multi = new int[3];
+        
+        for (int i = 1; i < n; i++) {
+            multi[0] = res[next[0]] * 2;
+            multi[1] = res[next[1]] * 3;
+            multi[2] = res[next[2]] * 5;
+            int ugly = Math.min(multi[0], Math.min(multi[1], multi[2]));
+            if (ugly == multi[0]) {
+                next[0]++;
+            }
+            if (ugly == multi[1]) {
+                next[1]++;
+            }
+            if (ugly == multi[2]) {
+                next[2]++;
+            }
+            res[i] = ugly;
+        }
+        return res[n - 1];
+    }
+    
+    
+    public int nthUglyNumber(int n) {
+        int[] res = new int[n];
+        res[0] = 1;
+        int[] next = new int[3];
+        int[] multi = new int[3];
+        int[] primes = {2,3,5};
+        
+        int ugly = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            for(int j = 0; j < 3; j++) {
+                multi[j] = res[next[j]] * primes[j];
+                if(multi[j] < ugly) {
+                    ugly = multi[j];
+                }
+            }
+            for(int j = 0; j < 3; j++) {
+                if(ugly == multi[j]) {
+                    next[j]++;
+                }
+            }
+            res[i] = ugly;
+            ugly = Integer.MAX_VALUE;
+        }
+        return res[n - 1];
+    }
