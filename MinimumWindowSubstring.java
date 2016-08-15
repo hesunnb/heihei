7f1864fce4a2055ef9936dac140f8187bd228b81
hesunnb <hesun63@gmail.com>
2016-08-14 22:04:07 -0400

    Update MinimumWindowSubstring.java

--- MinimumWindowSubstring.java
+++ MinimumWindowSubstring.java
@@ -18,14 +18,60 @@ For source = "ADOBECODEBANC", target = "ABC", the minimum window is "BANC"
 */
 
 public class Solution {
+    
+    //复杂度O(n), 且只用了一个数组
+    public String minWindow(String s, String t) {
+        
+        if(s == null || t == null || s.length() == 0) {
+            return "";
+        }
+        
+        int[] thash = new int[256];
+        int len = Integer.MAX_VALUE;
+        int snumber = 0;
+        String minStr = "";
+        
+        initThash(thash, t);
+        int i = 0;
+        int j = 0;
+        for(i = 0; i < s.length(); i++) { //i在j的后面
+            if(thash[s.charAt(i)] > 0) {
+                snumber++; //记录有效字符出现了几次
+            }
+            thash[s.charAt(i)]--; //相应位置--, 记录s中字符出现的次数, 因为每次什么字母都--, 所以往回加的时候只有有效字符能够加到比0大, 所以上面用大于0判断
+            
+            while(snumber >= t.length()) { //说明现在为有效, j与i之间已经包含了t字符串
+                if(len > i - j + 1) {
+                    len = i - j + 1;
+                    minStr = s.substring(j, i + 1); //先取子串
+                }
+                
+                thash[s.charAt(j)]++; //去掉j处的字符, 以后不计入了
+                if(thash[s.charAt(j)] > 0) { //去掉j处字符后, 如果该处值大于0, 说明去除掉的是有效字符
+                    snumber--; //snumber--就会小于t.length(), 然后i继续后移寻找有效字符
+                }
+                j++; //j在有效范围内每次都要后移缩小window的范围
+            }
+        }
+        return minStr;
+    }
+    
+    private void initThash(int[] thash, String t) {
+        for(int i = 0; i < t.length(); i++) {
+            thash[t.charAt(i)]++;
+        }
+    }
+}
+
+public class Solution {
     /**
      * @param source: A string
      * @param target: A string
      * @return: A string denote the minimum window
      *          Return "" if there is no such a string
      */
-     
-    //复杂度: O(n^2)
+    
+    //复杂度: O(n^2), 用了两个数组
     public String minWindow(String source, String target) {
         // write your code
         if(source == null || target == null || source.length() == 0) {
