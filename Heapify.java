81072e0b4227b5bd2e37c0de4de46e6a2eec72fe
hesunnb <hesun63@gmail.com>
2016-08-25 19:42:36 -0400

    Update Heapify.java

--- Heapify.java
+++ Heapify.java
@@ -58,3 +58,31 @@ public class Solution {
         }
     }
 }
+
+
+// Version 2: This cost O(nlogn)
+public class Solution {
+    /**
+     * @param A: Given an integer array
+     * @return: void
+     */
+    private void siftup(int[] A, int k) {
+        while (k != 0) {
+            int father = (k - 1) / 2;
+            if (A[k] > A[father]) {
+                break;
+            }
+            int temp = A[k];
+            A[k] = A[father];
+            A[father] = temp;
+            
+            k = father;
+        }
+    }
+    
+    public void heapify(int[] A) {
+        for (int i = 0; i < A.length; i++) {
+            siftup(A, i);
+        }
+    }
+}
