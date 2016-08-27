224ae2a548940318c6667e36059293dbee981356
hesunnb <hesun63@gmail.com>
2016-08-26 23:33:59 -0400

    Update LinkedListCycleII.java

--- LinkedListCycleII.java
+++ LinkedListCycleII.java
@@ -16,7 +16,77 @@ Given -21->10->4->5, tail connects to node index 1，return 10*/
  *     }
  * }
  */ 
- 
+
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The node where the cycle begins. 
+     *           if there is no cycle, return null
+     */
+    
+    //快慢指针都在head出发
+    public ListNode detectCycle(ListNode head) {  
+        // write your code here
+        
+        if(head == null || head.next == null) {
+            return null;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                ListNode slow2 = head; //让第二个slow指针从head出发
+                while(slow2 != slow) {
+                    slow2 = slow2.next; //slow2与slow同时走, 碰到的时候就是环的起始位置
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        
+        return null;
+    }
+    
+    
+    //快指针在慢指针的后一点出发
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next==null) {
+            return null;
+        }
+
+        ListNode fast, slow;
+        fast = head.next;
+        slow = head;
+        while (fast != slow) {
+            if(fast==null || fast.next==null)
+                return null;
+            fast = fast.next.next;
+            slow = slow.next;
+        } 
+        
+        while (head != slow.next) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+    }
+}
+
+
 //开哈希表
 public class Solution {
     /**
