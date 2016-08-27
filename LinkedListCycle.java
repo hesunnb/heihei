f97e0e9a5a3bcfdab31809dd679d7fe9c2444608
hesunnb <hesun63@gmail.com>
2016-08-26 21:44:38 -0400

    Update LinkedListCycle.java

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -3,6 +3,46 @@
 Follow up:
 Can you solve it without using extra space?*/
 
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+ 
+//不开空间快慢指针
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {  
+        // write your code here
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode fast, slow;
+        fast = head.next;
+        slow = head;
+        while (fast != slow) {
+            if(fast==null || fast.next==null) {
+                return false;
+            }
+                
+            fast = fast.next.next;
+            slow = slow.next;
+        } 
+        return true;
+    }
+}
+
+
 //开空间用哈希表
 /**
  * Definition for singly-linked list.
