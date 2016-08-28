da8c50375db9ffa4a788d1315d91d94585c37b45
hesunnb <hesun63@gmail.com>
2016-08-28 19:27:51 -0400

    Create FlattenList.java

---
+++ FlattenList.java
@@ -0,0 +1,5 @@
+/*Given a list, each element in the list can be a list or integer. flatten it into a simply list with integers.
+
+ Notice
+
+If the element in the given list is a list, it can contain list too.*/
