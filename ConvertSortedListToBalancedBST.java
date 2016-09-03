628a8e8262921b2eb5a9cdf68961c01f0fbd76eb
hesunnb <hesun63@gmail.com>
2016-09-03 10:41:42 -0400

    Update ConvertSortedListToBalancedBST.java

--- ConvertSortedListToBalancedBST.java
+++ ConvertSortedListToBalancedBST.java
@@ -27,6 +27,7 @@ Example
  */ 
  
 //开空间然后是ConvertSortedArrayToBST的写法
+//时间O(n)
 public class Solution {
     /**
      * @param head: The first node of linked list.
@@ -59,5 +60,42 @@ public class Solution {
         node.left = buildTree(list, start, mid - 1);
         node.right = buildTree(list, mid + 1, end);
         return node;
+    }
+}
+
+
+//不开空间, 每次从头去找每段分下来的链表的中点
+//时间O(nlogn)
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+        
+        return helper(head, null);
+    }
+    
+    private TreeNode helper(ListNode head, ListNode tail) {
+        if(head == tail) { //对于-1->0->1->3->null例子, 就会有head==tail的情况, 此时就要返回null, 否则就互相连就乱了
+            return null;
+        }
+        if(head.next == tail) { //只剩下一个节点的时候, 就开始连接了
+            return new TreeNode(head.val);
+        }
+        ListNode mid = findMiddle(head, tail); //找中点
+        TreeNode root = new TreeNode(mid.val);
+        root.left = helper(head, mid);
+        root.right = helper(mid.next, tail);
+        return root;
+    }
+    
+    private ListNode findMiddle(ListNode head, ListNode tail) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != tail && fast.next != tail) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
