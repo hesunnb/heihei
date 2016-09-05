9965a4cec03111a9e1a99696cdfbfb7e656fc977
hesunnb <hesun63@gmail.com>
2016-09-05 12:25:58 -0400

    Update subtree.java

--- subtree.java
+++ subtree.java
@@ -30,7 +30,6 @@ T1 = 2   3       T2 =    4
  *     }
  * }
  */
- 
 public class Solution {
     /**
      * @param T1, T2: The roots of binary tree.
@@ -38,7 +37,6 @@ public class Solution {
      */
     public boolean isSubtree(TreeNode T1, TreeNode T2) {
         // write your code here
-        
         if (T2 == null) { //null是任何树的子树
             return true;
         }
@@ -46,15 +44,15 @@ public class Solution {
             return false;
         }
         
-        if (isEqual(T1, T2)) {
+        if (isIdentical(T1, T2)) {
             return true;
         }
-        if (isSubtree(T1.left, T2) || isSubtree(T1.right, T2)) { //左子树与右子树有一个和T2一样的, 就返回真; 短路或, 有一个为真递归就终止了
+        if (isSubtree(T1.left, T2) || isSubtree(T1.right, T2)) { //左子树与右子树有一个和T2一样的, 就返回真; 短路或, 
+        //有一个为真递归就终止了
             return true;
         }
         return false;
     }
-    
     
     private boolean isIdentical(TreeNode a, TreeNode b) {
         // Write your code here
