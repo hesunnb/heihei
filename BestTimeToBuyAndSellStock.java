dd8b9d794658f0ba69638ca876849925527a4666
hesunnb <hesun63@gmail.com>
2016-09-10 19:20:33 -0400

    Create BestTimeToBuyAndSellStock.java

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,44 @@
+/*Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.*/
+
+public class Solution {
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+     
+    /*O(n^2), 与longest increasing sequence的O(n^2)解法一样, 超时*/
+    public int maxProfit(int[] prices) {
+        // write your code here
+        
+        if(prices == null || prices.length == 0) {
+            return 0;
+        }
+        
+        int[] count = new int[prices.length];
+        int max = 0;
+        for(int i = 0; i < prices.length; i++) {
+            count[i] = 0;
+            for(int j = 0; j < i; j++) {
+                if(prices[i] > prices[j]) {
+                    count[i] = Math.max(count[i], prices[i] - prices[j]);
+                }
+                max = Math.max(max, count[i]);
+            }
+        }
+        
+        return max;
+    }
+}
