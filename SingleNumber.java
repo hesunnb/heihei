f18338abbfb595545729e870a1da4156951f230d
hesunnb <hesun63@gmail.com>
2016-09-10 09:04:58 -0400

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,5 +1,8 @@
 /*Given an array of integers, every element appears twice except for one. Find that single one.
 
+Example
+Given [1,2,2,1,3,4,3], return 4
+
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?*/
 
