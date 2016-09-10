03bf715d08945002ead585183940deb64356ef81
hesunnb <hesun63@gmail.com>
2016-09-10 09:29:46 -0400

    Update SingleNumber3.java

--- SingleNumber3.java
+++ SingleNumber3.java
@@ -1,4 +1,5 @@
-/* Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+/* Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. 
+Find the two elements that appear only once.
 
 For example:
 
@@ -14,11 +15,13 @@ Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
 /*
 explanation:
 
-In the first pass, we XOR all elements in the array, and get the XOR of the two numbers we need to find. Note that since the two numbers are distinct, so there must be a set bit (that is, the bit with value '1') in the XOR result. Find
-    out an arbitrary set bit (for example, the rightmost set bit).
+In the first pass, we XOR all elements in the array, and get the XOR of the two numbers we need to find. Note that since the 
+two numbers are distinct, so there must be a set bit (that is, the bit with value '1') in the XOR result. Find
+out an arbitrary set bit (for example, the rightmost set bit).
 
-In the second pass, we divide all numbers into two groups, one with the aforementioned bit set, another with the aforementinoed bit unset. Two different numbers we need to find must fall into thte two distrinct groups. XOR numbers in each group, we can find a number in either group.
-
+In the second pass, we divide all numbers into two groups, one with the aforementioned bit set, 
+another with the aforementinoed bit unset. Two different numbers we need to find must fall into thte two distrinct groups. 
+XOR numbers in each group, we can find a number in either group.
 */
 
 public class Solution {
