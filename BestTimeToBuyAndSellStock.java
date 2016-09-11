89bb1f25252b5c2621bbc5858a88cf90abc37e25
hesunnb <hesun63@gmail.com>
2016-09-10 22:29:21 -0400

    Update BestTimeToBuyAndSellStock.java

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -13,6 +13,31 @@ Output: 0
 
 In this case, no transaction is done, i.e. max profit = 0.*/
 
+//题意就是在一个数组中找到后面减前面差值最大的两个数
+public class Solution {
+    
+    /*O(n)扫一遍就可以了*/
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) {
+            return 0;
+        }
+        
+        int min = Integer.MAX_VALUE; //min保存访问过的最小值
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++) {
+            if(prices[i] < min) { //小于最小值就要更新最小值
+                min = prices[i];
+            }
+            if(profit < prices[i] - min) { //大于profit就更新profit
+                profit = prices[i] - min;
+            }
+        }
+        
+        return profit;
+    }
+}
+
+
 public class Solution {
     /**
      * @param prices: Given an integer array
