9f6837e2a101d149d4db9d54a0901358b32290c9
hesunnb <hesun63@gmail.com>
2016-11-07 17:33:54 -0500

    Update StrStr.java

--- StrStr.java
+++ StrStr.java
@@ -1,4 +1,7 @@
 /*Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.*/
+/*For a given source string and a target string, you should output the first index(from 0) of target string in source string.
+
+If target does not exist in source, just return -1.*/
 
 public class Solution {
     public int strStr(String haystack, String needle) {
