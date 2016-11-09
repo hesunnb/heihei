232b95694174eb7f247d496e9b40c85cea0b6335
hesunnb <hesun63@gmail.com>
2016-11-08 21:06:30 -0500

    Update ConvertExpressionToReversePolishNotation.java

--- ConvertExpressionToReversePolishNotation.java
+++ ConvertExpressionToReversePolishNotation.java
@@ -1,3 +1,8 @@
+/*Given an expression string array, return the Reverse Polish notation of this expression. (remove the parentheses)
+Example
+For the expression [3 - 4 + 5] (which denote by ["3", "-", "4", "+", "5"]), return [3 4 - 5 +] (which denote by ["3", "4", "-", "5", "+"])
+*/
+
 public class Solution {
     /**
      * @param expression: A string array
