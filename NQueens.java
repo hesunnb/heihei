3737b8db6a5b2314dad4f4724a678367318e0567
hesunnb <hesun63@gmail.com>
2017-04-16 20:29:55 -0400

    Update NQueens.java

--- NQueens.java
+++ NQueens.java
@@ -60,7 +60,8 @@ class Solution {
             if(isValid(row, index, i)) { //横坐标是index, 纵坐标是i这个点如果有效
                 row[index] = i; //在横坐标的地方记录纵坐标, 用一个数组完成二维数组记录有效位置的功能
                 solveNQueensCore(row, index + 1, n, result); //当前行如果该位置有效, 就去找下一行的有效位置(下一行就是index + 1)
-                row[index] = 0; //solveNQueensCore返回之后, 说明当前点的所有可能结果已经递归完, 这时候要把当前点的位置清0, 然后继续扫描当前行的下一个点
+                row[index] = 0; //solveNQueensCore返回之后, 说明当前点的所有可能结果已经递归完, 这时候要把当前点的位置清0, 
+                //然后继续扫描当前行的下一个点
             }
         }
     }
@@ -82,12 +83,14 @@ class Solution {
     }
     
     private boolean isValid(int[] row, int rowNum, int colNum) {
-        for(int i = 0; i < rowNum; i++) { //比如传过来(3,1)这个点, rowNum=3, colNum=1, 那么就要看看rowNum在row这个数组里面前方的位置列的情况, 因为在row中rowNum前方存下的位置都是有效位置
+        for(int i = 0; i < rowNum; i++) { //比如传过来(3,1)这个点, rowNum=3, colNum=1, 那么就要看看rowNum在row这个数组里面前方的位置列的情况, 
+        //因为在row中rowNum前方存下的位置都是有效位置
             if(row[i] == colNum) {
                 return false; //比如i=0,row[i]=1,说明在[0,1]这个位置已经有皇后了, 所以(3,1)就无效, 列不能重复, 就返回假
                 //因为是每行每行扫, 所以行不会存在重复, 正在扫描的要放值的这行现在是没有值的, 所以不用判断行重复
             }
-            if(Math.abs(i - rowNum) == Math.abs(row[i] - colNum)) { //判断斜着的行有没有重复, 行行与列列的差值如果相等就说明在同一个斜着的方向上, 包括左斜和右斜
+            if(Math.abs(i - rowNum) == Math.abs(row[i] - colNum)) { //判断斜着的行有没有重复, 行行与列列的差值如果相等就说明在同一个斜着
+            //的方向上, 包括左斜和右斜
                 return false;
             }
         }
