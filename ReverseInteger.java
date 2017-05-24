fa537b2a9259eebb9cc970019f4183459b6d41b5
hesunnb <hesun63@gmail.com>
2017-05-24 11:51:26 -0400

    Update and rename Reverse Integer.java to ReverseInteger.java

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -13,7 +13,8 @@ public class Solution {
             int next_rst = rst * 10 + n % 10; //把尾数提取出来，存下
             n = n / 10; //把尾数撇掉
             if(next_rst/10 != rst) //这句就是看看有没有越界发生，发生啦截取后的值除以10以后肯定和原来的不一样
-            //int的值在计算的情况下，结果大于intmax会截取比如int n = 123456789 * 123456789，会截取；但是如果直接int n = 123456789999L, 就是把long给int，是不可以哒
+            //int的值在计算的情况下，结果大于intmax会截取比如int n = 123456789 * 123456789，会截取；但是如果直接int n = 123456789999L, 
+	    //就是把long给int，是不可以哒
             {
                 rst  = 0;
                 break;
@@ -23,33 +24,33 @@ public class Solution {
         return rst;
         
         
-		/*String result = "";
-		String temp = String.valueOf(n);
-		Stack<Character> st = new Stack<Character>();
-		
-		for(int i = 0; i < temp.length(); i++)
+	/*String result = "";
+	String temp = String.valueOf(n);
+	Stack<Character> st = new Stack<Character>();
+
+	for(int i = 0; i < temp.length(); i++)
+	{
+		if(temp.charAt(i) != '-')
 		{
-			if(temp.charAt(i) != '-')
-			{
-				st.push(temp.charAt(i));
-			}
+			st.push(temp.charAt(i));
 		}
-		if(n < 0)
-		{
-			result += "-";
-		}
-		while(!st.isEmpty())
-		{
-			result += st.pop();
-		}
-		long val = Long.parseLong(result);
-		if(val > Integer.MAX_VALUE || val < Integer.MIN_VALUE)
-		{
-		    return 0;
-		}
-		else
-		{
-		    return Integer.parseInt(result);
-		}*/
+	}
+	if(n < 0)
+	{
+		result += "-";
+	}
+	while(!st.isEmpty())
+	{
+		result += st.pop();
+	}
+	long val = Long.parseLong(result);
+	if(val > Integer.MAX_VALUE || val < Integer.MIN_VALUE)
+	{
+	    return 0;
+	}
+	else
+	{
+	    return Integer.parseInt(result);
+	}*/
     }
 }
