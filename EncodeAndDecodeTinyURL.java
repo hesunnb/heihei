08fc72052539924c318694eab89ff8a93fb1d4aa
hesunnb <hesun63@gmail.com>
2018-01-11 17:44:20 -0500

    Create EncodeAndDecodeTinyURL.java

---
+++ EncodeAndDecodeTinyURL.java
@@ -0,0 +1,42 @@
+/*TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns 
+a short URL such as http://tinyurl.com/4e9iAk.
+
+Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. 
+You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.*/
+
+public class Codec {
+
+    Map<String, String> index = new HashMap<String, String>();
+    Map<String, String> revIndex = new HashMap<String, String>();
+    String BASE_HOST = "http://tinyurl.com/";
+    
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        if (revIndex.containsKey(longUrl)) {
+            return BASE_HOST + revIndex.get(longUrl);
+        } //如果在revIndex已经有longUrl, 直接返回其相应的shortUrl
+        
+        String charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        String key = null;
+        do {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 6; i++) {
+                int r = (int) (Math.random() * charSet.length());
+                sb.append(charSet.charAt(r));
+            }
+            key = sb.toString();
+        } while (index.containsKey(key)); //doWhile循环是while为真进行循环, 先加进去一个key, 如果这个生成的key已经存在就继续生成
+        index.put(key, longUrl); //用于decode
+        revIndex.put(longUrl, key); //用于encode
+        return BASE_HOST + key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return index.get(shortUrl.replace(BASE_HOST, "")); //把BASE_HOST替换掉取出longUrl
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
