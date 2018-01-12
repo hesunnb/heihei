898a6b734b6e28a291655101105b90f95a73b12f
hesunnb <hesun63@gmail.com>
2018-01-12 16:38:02 -0500

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,6 +1,6 @@
 /*Determine whether an integer is a palindrome. Do this without extra space.*/
 
-//solution1
+//solution2
 class Solution {
     
     //负数不是回文数
@@ -13,7 +13,7 @@ class Solution {
         }
         int start = 0, end = s.length() - 1;
 
-        while(start < end) { //判断起来比ValidPalindrome那道题还简单, 直接转换成字符串从头到尾走一遍就行
+        while(start < end) { //判断起来比ValidPalindrome那道题还简单, 直接转换成字符串从头到尾走一遍就行, 但是在这里新建一个字符串都算extra space
             if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
                 return false;
             }
