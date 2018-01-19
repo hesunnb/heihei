75db2e3dbb70ddb76e3821e07b95265272047393
hesunnb <hesun63@gmail.com>
2018-01-19 17:30:07 -0500

    Update WordPattern.java

--- WordPattern.java
+++ WordPattern.java
@@ -12,7 +12,34 @@ You may assume pattern contains only lowercase letters, and str contains lowerca
 
 class Solution {
 
-    //solution2:(own), 与Isomorphic Strings同构字符串这道题意思非常接近, 属于类型题
+    
+    
+    
+    //solution2:(使用了Integer i进行循环, 避免了autoboxing-same-value-to-different-objects-problem的问题)
+    public boolean wordPattern(String pattern, String str) {
+        if(pattern == null || str == null) {
+            return false;
+        }
+        
+        String[] sArr = str.split(" ");
+        if(sArr.length != pattern.length()) {
+        	return false;
+        }
+        
+        Map map = new HashMap();
+        //这里循环必须使用Integer i, int i不行, 即使规定了map的泛型<Object, Integer>也不行, 因为使用int i的时候, 放入哈希表中的时候会出现
+        //autoboxing-same-value-to-different-objects-problem即相同值自动打包成了不同的对象, 而!=比较的是对象地址, 就会出错, 使用Integer的时候,
+        //相同的值就会是同一个整型常量, 所以它们的地址也相同就ok
+        for(Integer i = 0; i < pattern.length(); i++) {
+            if(map.put(pattern.charAt(i), i) != map.put(sArr[i], i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    //solution3:(own), 与Isomorphic Strings同构字符串这道题意思非常接近, 属于类型题
     public boolean wordPattern(String pattern, String str) {
         
         if(pattern == null || str == null) {
