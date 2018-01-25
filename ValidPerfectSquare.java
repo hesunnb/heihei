70c100fae4f00a13b6c9210de68dd36776a01d4e
hesunnb <hesun63@gmail.com>
2018-01-25 10:24:44 -0500

    Update ValidPerfectSquare.java

--- ValidPerfectSquare.java
+++ ValidPerfectSquare.java
@@ -13,7 +13,7 @@ Returns: False*/
 
 class Solution {
     
-    //复杂度是O(sqrt(n)), n=1计算1次, n=4计算2次, n=9计算3次, n=16级计算4次, 每次都是计算根号n次
+    //solution1: 复杂度是O(sqrt(n)), n=1计算1次, n=4计算2次, n=9计算3次, n=16级计算4次, 每次都是计算根号n次
     public boolean isPerfectSquare(int num) {
         if(num <= 0) {
             return false;
@@ -35,4 +35,26 @@ class Solution {
     36 = 1 + 3 + 5 + 7 + 9 + 11
     ....
     so 1+3+...+(2n-1) = (2n-1 + 1)n/2 = nn*/
+    
+    
+    
+    //solution2: binary search time complexity is O(log(n))
+    public boolean isPerfectSquare(int num) {
+        if(num <= 0) {
+            return false;
+        }
+        
+        long low = 1, high = num; //low是0是1都可以
+        while (low <= high) {
+            long mid = (low + high) / 2; //因为num是positive number, 所以(low+high) >>> 1逻辑右移也可
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
 }
