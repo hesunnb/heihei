3e2f33e5124bd51f458eaf536e3cdb2d34e25626
hesunnb <hesun63@gmail.com>
2018-01-31 11:06:36 -0500

    Update AddStrings.java

--- AddStrings.java
+++ AddStrings.java
@@ -14,37 +14,37 @@ class Solution {
         if(num1 == null || num2 == null) {
             return null;
         }
+        
         StringBuilder sb1 = new StringBuilder(num1);
-
         StringBuilder sb2 = new StringBuilder(num2);
         StringBuilder result = new StringBuilder();
         int carry = 0;
         int remain = 0;
         int sum = 0;
         while(!sb1.toString().equals("") && !sb2.toString().equals("")) {
-            sum = sb1.charAt(sb1.length() - 1) - '0' + sb2.charAt(sb2.length() - 1) - '0' + carry;
+            sum = sb1.charAt(sb1.length() - 1) - '0' + sb2.charAt(sb2.length() - 1) - '0' + carry; //加差值加进位
             carry = sum / 10;
             remain = sum % 10;
             result.insert(0, remain);
-            sb1.replace(sb1.length() - 1, sb1.length(), "");
+            sb1.replace(sb1.length() - 1, sb1.length(), ""); //移除最后一位
             sb2.replace(sb2.length() - 1, sb2.length(), "");
         }
         
-        while(!sb1.toString().equals("")) {
+        while(!sb1.toString().equals("")) { //当sb1还剩余值
             sum = sb1.charAt(sb1.length() - 1) - '0' + carry;
             carry = sum / 10;
             remain = sum % 10;
             result.insert(0, remain);
             sb1.replace(sb1.length() - 1, sb1.length(), "");
         }
-        while(!sb2.toString().equals("")) {
+        while(!sb2.toString().equals("")) { //当sb2还剩余值
             sum = sb2.charAt(sb2.length() - 1) - '0' + carry;
             carry = sum / 10;
             remain = sum % 10;
             result.insert(0, remain);
             sb2.replace(sb2.length() - 1, sb2.length(), "");
         }
-        if(carry != 0) {
+        if(carry != 0) { //把最后一位进位加上
             result.insert(0, carry);
         }
         return result.toString();
