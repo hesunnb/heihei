2ac148c3512000d109d61651ce44671ba669a49f
hesunnb <hesun63@gmail.com>
2018-01-31 11:56:07 -0500

    Update AddStrings.java

--- AddStrings.java
+++ AddStrings.java
@@ -9,6 +9,39 @@ You must not use any built-in BigInteger library or convert the inputs to intege
 
 class Solution {
 
+    //solution1: 自己写的精简版
+    public String addStrings(String num1, String num2) {
+        if(num1 == null || num2 == null) {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int x = 0, y = 0;
+        for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0; i--, j--) { //即使num1或者num2是"", 也只会造成i,j小于0,
+            //然后就+0, 没有影响的
+            if(i < 0) {
+                x = 0; //这样就算下标小于0也能加0而不影响
+            } else {
+                x = num1.charAt(i) - '0';
+            }
+            
+            if(j < 0) {
+                y = 0;
+            } else {
+                y = num2.charAt(j) - '0';
+            }
+           
+            sb.insert(0, (x + y + carry) % 10);
+            carry = (x + y + carry) / 10;
+        }
+        
+        if(carry != 0) {
+            sb.insert(0, carry);
+        }
+        return sb.toString();
+    
+        
     //solution2:(own)
     public String addStrings(String num1, String num2) {
         if(num1 == null || num2 == null) {
