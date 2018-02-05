ae09947b6b0fd7098f5f58fc8d2d52ed84062ab3
hesunnb <hesun63@gmail.com>
2018-02-05 10:42:53 -0500

    Create SubstringWithConcatenationOfAllWords.java

---
+++ SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,19 @@
+/*You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s 
+that is a concatenation of each word in words exactly once and without any intervening characters.
+
+For example, given:
+s: "barfoothefoobarman"
+words: ["foo", "bar"]
+
+You should return the indices: [0,9].
+(order does not matter).*/
+
+
+class Solution {
+    /*"foobarfoofoothefoofoobarfooman"
+    ["foo","bar","foo"]
+    Expected answer: [0,3,15,18], 允许words里面有重复的字符串, 但是这些字串必须长度都相同*/
+    public List<Integer> findSubstring(String s, String[] words) {
+        
+    }
+}
