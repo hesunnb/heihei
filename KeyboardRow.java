9e815a10ddcfe1ca7fdd2d0002494f101b481044
hesunnb <hesun63@gmail.com>
2018-02-23 16:39:23 -0500

    Update KeyboardRow.java

--- KeyboardRow.java
+++ KeyboardRow.java
@@ -15,6 +15,43 @@ You may assume the input string will only contain letters of alphabet.*/
 
 class Solution {
 
+    //solution1:用哈希表存row数好一点, 查询能更快一些
+    public String[] findWords(String[] words) {
+        
+        if(words == null || words.length == 0) {
+            return new String[0];
+        }
+        
+        String[] strs = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+        Map<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < strs.length; i++){
+            for(char c : strs[i].toCharArray()){
+                map.put(c, i); //put <char, rowIndex> pair into the map, 字母与行数组成哈希表
+            }
+        }
+        
+        List<String> res = new ArrayList<>();
+        for(String w : words){
+            if(w.equals("")) continue; //这个还真不能少, 因为如果map里面要是没有"", 那么get("")得到的就是null, 下面int index = null就报错了
+            int index = map.get(w.toLowerCase().charAt(0)); //根据第一个字母来看这个字符串应该属于哪个row
+            for(char c : w.toLowerCase().toCharArray()) {
+                if(map.get(c) != index) {
+                    index = -1; //don't need a boolean flag. 
+                    break;
+                }
+            }
+            
+            if(index != -1) { //if index != -1, this is a valid string
+                res.add(w);
+            }
+        }
+        
+        return res.toArray(new String[0]);
+    }
+    
+    
+    //solution3:(own)
     public String[] findWords(String[] words) {
         
         if(words == null || words.length == 0) {
