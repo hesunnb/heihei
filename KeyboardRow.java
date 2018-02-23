18a08202ce8fab55781d0c2c7e91683f032cde2b
hesunnb <hesun63@gmail.com>
2018-02-23 14:30:11 -0500

    Create KeyboardRow.java

---
+++ KeyboardRow.java
@@ -0,0 +1,61 @@
+/*Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
+
+
+American keyboard
+
+
+Example 1:
+Input: ["Hello", "Alaska", "Dad", "Peace"]
+Output: ["Alaska", "Dad"]
+Note:
+You may use one character in the keyboard more than once.
+You may assume the input string will only contain letters of alphabet.*/
+
+
+class Solution {
+
+    public String[] findWords(String[] words) {
+        
+        List<String> list = new ArrayList<>();
+        if(words == null || words.length == 0) {
+            return new String[0];
+        }
+        
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+        
+        for(int i = 0; i < words.length; i++) {
+            int j = 0;
+            for(j = 0 ; j < words[i].length(); j++) { //扫描row1
+                if(!row1.contains(String.valueOf(Character.toLowerCase(words[i].charAt(j))))) break;
+            }
+            if(j == words[i].length()) { //如果扫到尾了, 说明是
+                list.add(words[i]);
+                continue;
+            }
+            
+            for(j = 0 ; j < words[i].length(); j++) { //一样
+                if(!row2.contains(String.valueOf(Character.toLowerCase(words[i].charAt(j))))) break;
+            }
+            if(j == words[i].length()) {
+            	list.add(words[i]);
+                continue;
+            }
+            
+            for(j = 0 ; j < words[i].length(); j++) { //一样
+                if(!row3.contains(String.valueOf(Character.toLowerCase(words[i].charAt(j))))) break;
+            }
+            if(j == words[i].length()) {
+            	list.add(words[i]);
+                continue;
+            }
+        }
+        
+        String[] result = new String[list.size()];
+        for(int i = 0; i < result.length; i++) {
+        	result[i] = list.get(i);
+        }
+        return result;
+    }
+}
