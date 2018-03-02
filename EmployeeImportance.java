a0ce752686109c092e3fb8a99dddb8a930aa92ab
hesunnb <hesun63@gmail.com>
2018-03-02 10:46:23 -0500

    Update EmployeeImportance.java

--- EmployeeImportance.java
+++ EmployeeImportance.java
@@ -1,8 +1,8 @@
 /*You are given a data structure of employee information, which includes the employee's unique id, his importance value and his direct 
 subordinates' id.
 
-For example, employee 1 is the leader of employee 2, and employee 2 is the leader of employee 3. They have importance value 15, 10 and 5, 
-respectively. Then employee 1 has a data structure like [1, 15, [2]], and employee 2 has [2, 10, [3]], and employee 3 has [3, 5, []]. 
+For example, employee 1 is the leader of employee 2, and employee 2 is the leader of employee 3. They have importance value 15, 10 and 5
+, respectively. Then employee 1 has a data structure like [1, 15, [2]], and employee 2 has [2, 10, [3]], and employee 3 has [3, 5, []]. 
 Note that although employee 3 is also a subordinate of employee 1, the relationship is not direct.
 
 Now given the employee information of a company, and an employee id, you need to return the total importance value of this employee and 
@@ -52,6 +52,32 @@ class Solution {
         int total = root.importance; //total不用跟着传, 直接加上返回值就可以了
         for (int subordinate : root.subordinates) {
             total += getImportanceHelper(map, subordinate);
+        }
+        return total;
+    }
+    
+    
+    //solution2:bfs, 因为除了哈希表之外还用了一个队列, 所以不首选
+    public int getImportance(List<Employee> employees, int id) {
+        
+        if(employees == null || employees.size() == 0) {
+            return 0;
+        }
+        
+        int total = 0;
+        Map<Integer, Employee> map = new HashMap<>();
+        for (Employee employee : employees) {
+            map.put(employee.id, employee);
+        }
+        
+        Queue<Employee> queue = new LinkedList<>();
+        queue.offer(map.get(id));
+        while (!queue.isEmpty()) {
+            Employee current = queue.poll();
+            total += current.importance;
+            for (int subordinate : current.subordinates) {
+                queue.offer(map.get(subordinate));
+            }
         }
         return total;
     }
