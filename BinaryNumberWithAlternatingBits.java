8eea6344a8d5a2ed3db5ebe63c7b7d674d61a2a0
hesunnb <hesun63@gmail.com>
2018-04-09 10:06:06 -0400

    Update BinaryNumberWithAlternatingBits.java

--- BinaryNumberWithAlternatingBits.java
+++ BinaryNumberWithAlternatingBits.java
@@ -22,6 +22,29 @@ Explanation:
 The binary representation of 10 is: 1010.*/
 
 class Solution {
+    
+    //solution2:
+    public boolean hasAlternatingBits(int n) {
+        
+        //0和1都算真
+        if(n < 0) {
+            return false;
+        }
+        
+        /*
+        n =         1 0 1 0 1 0 1 0
+        n >> 1      0 1 0 1 0 1 0 1
+        n ^ n>>1    1 1 1 1 1 1 1 1
+        n           1 1 1 1 1 1 1 1
+        n + 1     1 0 0 0 0 0 0 0 0
+        n & (n+1)   0 0 0 0 0 0 0 0
+        */
+
+        n = n ^ (n>>1);
+        return (n & n+1) == 0;
+    }
+    
+    //solution3:
     public boolean hasAlternatingBits(int n) {
         
         //0和1都算真
