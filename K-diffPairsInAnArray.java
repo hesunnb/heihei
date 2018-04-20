66c8d65fa47e4f397ea7193799fb986a08f674b3
hesunnb <hesun63@gmail.com>
2018-04-20 10:57:04 -0400

    Update K-diffPairsInAnArray.java

--- K-diffPairsInAnArray.java
+++ K-diffPairsInAnArray.java
@@ -51,7 +51,25 @@ class Solution {
     }
     
     
-    //solution2:(own), 遍历数组的方法
+    //solution2:双指针
+    public int findPairs(int[] nums, int k) {
+        
+        if(nums == null || nums.length == 0 || k < 0) {
+            return 0;
+        }
+        
+        int ans = 0;
+        Arrays.sort(nums);
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            for (j = Math.max(j, i + 1); j < nums.length && (long) nums[j] - nums[i] < k; j++) ;
+            if (j < nums.length && (long) nums[j] - nums[i] == k) ans++;
+            while (i + 1 < nums.length && nums[i] == nums[i + 1]) i++;
+        }
+        return ans;
+    }
+    
+    
+    //solution3:(own), 遍历数组的方法
     public int findPairs(int[] nums, int k) {
         
         if(nums == null || nums.length == 0 || k < 0) {
