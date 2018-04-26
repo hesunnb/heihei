da6c69624c9988d8dab496064942d303abe6ee7d
hesunnb <hesun63@gmail.com>
2018-04-26 12:06:55 -0400

    Update SecondMinimumNodeInABinaryTree.java

--- SecondMinimumNodeInABinaryTree.java
+++ SecondMinimumNodeInABinaryTree.java
@@ -34,6 +34,40 @@ Explanation: The smallest value is 2, but there isn't any second smallest value.
  * }
  */
 class Solution {
+    
+    //solution1: dfs
+    public int findSecondMinimumValue(TreeNode root) {
+        
+        if(root == null) { //因为这道题限制了每个节点会有要么2个子节点, 要么0个子节点, 所以这个root为空的情况就只针对于最开始的根节点, 
+            //其余的时候用不到
+            return -1;
+        }
+        if(root.left == null && root.right == null) {
+            return -1; //返回-1的原因是所有节点都相等的时候直接返回-1方便
+        }
+
+        int left = root.left.val; //获取节点的左值
+        int right = root.right.val; //获取节点的右值
+
+        // if value same as root value, need to find the next candidate
+        if(root.left.val == root.val) { //相等的话就继续向下找, 不等的话就保留当前节点左右子节点的值
+            left = findSecondMinimumValue(root.left);
+        }
+        if(root.right.val == root.val) {
+            right = findSecondMinimumValue(root.right);
+        }
+
+        if(left != -1 && right != -1) {
+            return Math.min(left, right); //根节点左右子树都返回值的时候到这里决策
+        } else if(left != -1) { //这里左右不分先后, 因为上一步是左右都不为-1, 到这步肯定左右有一个为-1, 谁不为-1返回谁
+            return left;
+        } else {
+            return right;
+        }
+    }
+    
+    
+    //solution2: bfs(own), 要用到一个result[]数组和一个队列
     public int findSecondMinimumValue(TreeNode root) {
         
         int[] result = new int[2];
