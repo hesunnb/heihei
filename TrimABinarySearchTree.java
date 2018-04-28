51bdfac3eb4794ebb2caa5e3f5d40d2c78f5fddb
hesunnb <hesun63@gmail.com>
2018-04-28 17:03:19 -0400

    Update TrimABinarySearchTree.java

--- TrimABinarySearchTree.java
+++ TrimABinarySearchTree.java
@@ -56,17 +56,16 @@ class Solution {
         if(root == null) {
             return root;
         }
-        if(root.val > R) { //利用BST的性质, 如果root.val不在L与R之间, 那么就往下找, 如果找到了就new新的节点, 然后返回新的节点
+        if(root.val > R) { //利用BST的性质, 如果root.val不在L与R之间, 那么就往下找, 如果找到了就从找到的节点开始, 并返回找到的节点
             //如果一直找也找不到, 那么就会一直到root==null, 直接返回null了
             return trimBSTHelper(root.left, L, R);
         }
         if(root.val < L) {
             return trimBSTHelper(root.right, L, R);
         }
-        //如果root.val既不大于R, 又不小于L, 那么root.val一定>=L && <=R, 所以直接new新的节点就可以了
-        TreeNode node = new TreeNode(root.val);
-        node.left = trimBSTHelper(root.left, L, R);
-        node.right = trimBSTHelper(root.right, L, R);
-        return node;
+        //如果root.val既不大于R, 又不小于L, 那么root.val一定>=L && <=R, 所以操作这个节点就可以了
+        root.left = trimBSTHelper(root.left, L, R);
+        root.right = trimBSTHelper(root.right, L, R);
+        return root;
     }
 }
