12105a2b9830b55c6922c3a102e82eabd504715c
hesunnb <hesun63@gmail.com>
2018-05-06 19:20:58 -0400

    Update LongestUnivaluePath.java

--- LongestUnivaluePath.java
+++ LongestUnivaluePath.java
@@ -33,3 +33,38 @@ Output:
 2
 Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.*/
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int len = 0; // global variable
+    public int longestUnivaluePath(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        getLen(root, root.val); //最后一次, val == root.val就是根与根比较并且+1, 但是最后返回的是len, 所以说最后这个关于根的+1的返回值
+        //并没有加入到len, 就是没用到, 所以没事
+        return len;
+    }
+
+    private int getLen(TreeNode node, int val) {
+        if (node == null) {
+            return 0;
+        }
+        int left = getLen(node.left, node.val);
+        int right = getLen(node.right, node.val);
+        len = Math.max(len, left + right); //只有父子节点相等的时候才会返回非0值, 每个子树的根节点都是和底下的全部连通的, 所以要左右子树
+        //加和
+        if (val == node.val)  {
+            return Math.max(left, right) + 1; //左右选一个的原因是只能是单路径, 不能分叉, 见上图
+        }
+        return 0; //一旦父子节点不相等连不上, 就返回0
+    }
+}
+
