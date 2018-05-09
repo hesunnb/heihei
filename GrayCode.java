cad7051b0a043e7f135eaa6f8336ab4d51a9de52
hesunnb <hesun63@gmail.com>
2018-05-09 16:53:56 -0400

    Update GrayCode.java

--- GrayCode.java
+++ GrayCode.java
@@ -17,6 +17,8 @@ For example, [0,2,3,1] is also a valid gray code sequence according to the above
 For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.*/
 
 class Solution {
+    
+    //solution1:
     /*My idea is to generate the sequence iteratively. For example, when n=3, we can get the result based on n=2.
     00,01,11,10 -> (000,001,011,010) (110,111,101,100). The middle two numbers only differ at their highest bit, while the rest numbers 
     of part two are exactly symmetric of part one. It is easy to see its correctness.*/
@@ -33,6 +35,23 @@ class Solution {
             for(int k = size - 1; k >= 0; k--) {
                 result.add(result.get(k) | 1 << i); //从最后一位到第一位, 取出每一个数, 在它的最高位加入1
             }
+        }
+        return result;
+    }
+    
+    
+    //solution2:
+    /*The idea is simple. G(i) = i ^ (i/2).*/
+    //知道格雷码的规律在这里就可以, 面试的时候作为备选
+    public List<Integer> grayCode(int n) {
+        
+        List<Integer> result = new ArrayList<>();
+        if(n < 0) {
+            return result;
+        }
+        
+        for (int i = 0; i < 1<<n; i++) {
+            result.add(i ^ i>>1);
         }
         return result;
     }
