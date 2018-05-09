80d6fd30c124d280c3f060289295e0d6b7e1a59e
hesunnb <hesun63@gmail.com>
2018-05-09 17:10:25 -0400

    Update rotate image.java

--- rotate image.java
+++ rotate image.java
@@ -4,19 +4,17 @@ public class Solution {
      * @return: Void
      */
     
+    //因为是n*n, 所以才让do-inplace, 长宽不等没法inplace
     //思路就是记住是整个图像旋转，不是窜，是旋转
     public void rotate(int[][] matrix) {
         // write your code here
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
-        {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return; //虽然函数没有返回值，但是意思就是结束，不向下进行啦
         }
         
         int length = matrix.length;
-        for(int i = 0; i < length / 2; i++)
-        {
-            for(int j = 0; j < (length + 1) / 2; j++) //取循环的位置
-            {
+        for(int i = 0; i < length / 2; i++) {
+            for(int j = 0; j < (length + 1) / 2; j++) { //取循环的位置
                 int temp = matrix[i][j]; //取数旋转，旋转4回，依次填充相应位置
                 matrix[i][j] = matrix[length - j - 1][i]; //注意是先j后i
                 matrix[length - j - 1][i] = matrix[length - i - 1][length - j - 1];
