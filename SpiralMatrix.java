88ad40057aee76c343cbb395b6e844164ce98928
hesunnb <hesun63@gmail.com>
2018-05-11 10:31:42 -0400

    Create SpiralMatrix.java

---
+++ SpiralMatrix.java
@@ -0,0 +1,72 @@
+/*Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+
+Example 1:
+
+Input:
+[
+ [ 1, 2, 3 ],
+ [ 4, 5, 6 ],
+ [ 7, 8, 9 ]
+]
+Output: [1,2,3,6,9,8,7,4,5]
+Example 2:
+
+Input:
+[
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9,10,11,12]
+]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]*/
+
+class Solution {
+
+    //solution1: 稍有问题的就是下面多出来判断那里
+    public List<Integer> spiralOrder(int[][] matrix) {
+        
+        List<Integer> result = new ArrayList<Integer>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+        
+        int rowBegin = 0; //用四个位置限制住要遍历的矩阵
+        int rowEnd = matrix.length-1;
+        int colBegin = 0;
+        int colEnd = matrix[0].length - 1;
+        
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            // Traverse Right
+            for (int j = colBegin; j <= colEnd; j ++) {
+                result.add(matrix[rowBegin][j]);
+            }
+            rowBegin++;
+            
+            // Traverse Down
+            for (int j = rowBegin; j <= rowEnd; j ++) {
+                result.add(matrix[j][colEnd]);
+            }
+            colEnd--;
+            
+            if (rowBegin <= rowEnd) { //discuss里面说唯一不好的地方就是要在这里判断一下rowBegin <= rowEnd, 和下面colBegin <= colEnd
+                // Traverse Left
+                for (int j = colEnd; j >= colBegin; j --) {
+                    result.add(matrix[rowEnd][j]);
+                }
+            }
+            rowEnd--;
+            
+            if (colBegin <= colEnd) {
+                // Traver Up
+                for (int j = rowEnd; j >= rowBegin; j --) {
+                    result.add(matrix[j][colBegin]);
+                }
+            }
+            colBegin ++;
+        }
+        
+        return result;
+    }
+    
+    
+    //solution2:
+}
