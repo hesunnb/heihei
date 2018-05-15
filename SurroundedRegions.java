10a7910d7581e7da731db03a31a615b20d469aa6
hesunnb <hesun63@gmail.com>
2018-05-14 22:27:14 -0400

    Update SurroundedRegions.java

--- SurroundedRegions.java
+++ SurroundedRegions.java
@@ -37,56 +37,56 @@ class Solution {
              X O X X           X 1 X X             X O X X*/
     public void solve(char[][] board) {
 
-		if (board == null || board.length <= 2 || board[0].length <= 2) { //小于等于2的时候都是边, 没有包裹的时候存在
-			return;
-		}
+	if (board == null || board.length <= 2 || board[0].length <= 2) { //小于等于2的时候都是边, 没有包裹的时候存在
+		return;
+	}
 
-		int row = board.length;
-		int col = board[0].length;
+	int row = board.length;
+	int col = board[0].length;
 
-		for (int i = 0; i < row; i++) {
-			check(board, i, 0, row, col); //检查最左侧一列, 行在变而列不动
-			if (col > 1) {
-				check(board, i, col - 1, row, col); //检查最右侧一列
-			}
-		}
-		for (int j = 1; j + 1 < col; j++) { //检查最顶上一行, 不包括第一个值和最后一个值, 因为检查列的时候已经检查过了
-			check(board, 0, j, row, col);
-			if (row > 1) {
-                check(board, row - 1, j, row, col); //检查最底下一行
+	for (int i = 0; i < row; i++) {
+	    check(board, i, 0, row, col); //检查最左侧一列, 行在变而列不动
+	    if (col > 1) {
+		check(board, i, col - 1, row, col); //检查最右侧一列
+	    }
+	}
+	for (int j = 1; j + 1 < col; j++) { //检查最顶上一行, 不包括第一个值和最后一个值, 因为检查列的时候已经检查过了
+	    check(board, 0, j, row, col);
+	    if (row > 1) {
+		check(board, row - 1, j, row, col); //检查最底下一行
             }
-		}
-		for (int i = 0; i < row; i++) {
+	}
+	for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (board[i][j] == 'O') { //全矩阵把'O'变'X'
                     board[i][j] = 'X';
                 }
             }
         }
-		for (int i = 0; i < row; i++) {
+	for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (board[i][j] == '1') { //'1'变'O'
                     board[i][j] = 'O';
                 }
             }
         }
-	}
+    }
 
-	private void check(char[][] board, int i, int j, int row, int col) {
-		if (board[i][j] == 'O') { //因为是从四个边出发, 所以检查出'O'进行递归, 把跟这个'O'相连的所有'O'都找出来
-			board[i][j] = '1'; //把找到的相关的'O'都变成'1'
-			if (i > 1) {
-                check(board, i - 1, j, row, col); //上
-            }
-			if (j > 1) {
-                check(board, i, j - 1, row, col); //左
-            }
-			if (i + 1 < row) {
-                check(board, i + 1, j, row, col); //下
-            }
-			if (j + 1 < col) {
-                check(board, i, j + 1, row, col); //右
-            }
-		}
+    private void check(char[][] board, int i, int j, int row, int col) {
+	if (board[i][j] == 'O') { //因为是从四个边出发, 所以检查出'O'进行递归, 把跟这个'O'相连的所有'O'都找出来
+	    board[i][j] = '1'; //把找到的相关的'O'都变成'1'
+	    if (i > 1) {
+		check(board, i - 1, j, row, col); //上
+	    }
+	    if (j > 1) {
+		check(board, i, j - 1, row, col); //左
+	    }
+	    if (i + 1 < row) {
+		check(board, i + 1, j, row, col); //下
+	    }
+	    if (j + 1 < col) {
+		check(board, i, j + 1, row, col); //右
+	    }
 	}
+    }
 }
