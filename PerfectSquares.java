63027ad2765e0cfe3fd5080b504b95d773d225e0
hesunnb <hesun63@gmail.com>
2018-05-17 20:30:33 -0400

    Update PerfectSquares.java

--- PerfectSquares.java
+++ PerfectSquares.java
@@ -75,4 +75,82 @@ class Solution {
         int sqrtN = (int)(Math.sqrt(n));  
         return (sqrtN*sqrtN == n);  
     }
+	
+    
+    //solution3: BFS
+    public int numSquares(int n) {
+        
+        if (n <= 0) {
+            return 0;
+        }
+        
+        // perfectSquares contain all perfect square numbers which 
+        // are smaller than or equal to n.
+        List<Integer> perfectSquares = new ArrayList<>();
+        // cntPerfectSquares[i - 1] = the least number of perfect 
+        // square numbers which sum to i.
+        List<Integer> cntPerfectSquares = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            cntPerfectSquares.add(0);
+        }
+        
+        // Get all the perfect square numbers which are smaller than 
+        // or equal to n.
+        for (int i = 1; i*i <= n; i++) {
+            perfectSquares.add(i*i);
+            cntPerfectSquares.set(i*i - 1, 1);
+        }
+        
+        // If n is a perfect square number, return 1 immediately.
+        if (perfectSquares.get(perfectSquares.size() - 1) == n) {
+            return 1;
+        }
+        
+        // Consider a graph which consists of number 0, 1,...,n as
+        // its nodes. Node j is connected to node i via an edge if  
+        // and only if either j = i + (a perfect square number) or 
+        // i = j + (a perfect square number). Starting from node 0, 
+        // do the breadth-first search. If we reach node n at step 
+        // m, then the least number of perfect square numbers which 
+        // sum to n is m. Here since we have already obtained the 
+        // perfect square numbers, we have actually finished the 
+        // search at step 1.
+        Queue<Integer> searchQ = new LinkedList<>();
+        for (int i : perfectSquares) {
+            searchQ.offer(i);
+        }
+        
+        int currCntPerfectSquares = 1;
+        while (!searchQ.isEmpty()) {
+            currCntPerfectSquares++;
+            
+            int searchQSize = searchQ.size();
+            for (int i = 0; i < searchQSize; i++) {
+                int tmp = searchQ.peek();
+                // Check the neighbors of node tmp which are the sum 
+                // of tmp and a perfect square number.
+                for (int j : perfectSquares) {
+                    if (tmp + j == n) {
+                        // We have reached node n.
+                        return currCntPerfectSquares;
+                    }
+                    else if ((tmp + j < n) && (cntPerfectSquares.get(tmp + j - 1) == 0)) {
+                        // If cntPerfectSquares[tmp + j - 1] > 0, this is not 
+                        // the first time that we visit this node and we should 
+                        // skip the node (tmp + j).
+                        cntPerfectSquares.set(tmp + j - 1, currCntPerfectSquares);
+                        searchQ.offer(tmp + j);
+                    }
+                    else if (tmp + j > n) {
+                        // We don't need to consider the nodes which are greater ]
+                        // than n.
+                        break;
+                    }
+                }
+                searchQ.poll();
+            }
+        }
+        
+        return 0;
+    }
 }
