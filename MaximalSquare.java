b0a19861e4d0c3b835b4dfd21cb1259bfac50f75
hesunnb <hesun63@gmail.com>
2018-05-21 20:58:57 -0400

    Update MaximalSquare.java

--- MaximalSquare.java
+++ MaximalSquare.java
@@ -19,15 +19,15 @@ class Solution {
             return 0;
         }
       
-        int max = 0, n = matrix.length, m = matrix[0].length;
+        int max = 0, m = matrix.length, n = matrix[0].length;
 
         // dp(i, j) represents the length of the square 
         // whose lower-right corner is located at (i, j)
         // dp(i, j) = min{ dp(i-1, j-1), dp(i-1, j), dp(i, j-1) }
         int[][] dp = new int[n + 1][m + 1];
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
               if(matrix[i - 1][j - 1] == '1') {
                 dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                 max = Math.max(max, dp[i][j]);
