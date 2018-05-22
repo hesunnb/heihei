82cdd078b338ed6405207971bfce5fb6c980787a
hesunnb <hesun63@gmail.com>
2018-05-21 20:59:28 -0400

    Update MaximalSquare.java

--- MaximalSquare.java
+++ MaximalSquare.java
@@ -24,7 +24,7 @@ class Solution {
         // dp(i, j) represents the length of the square 
         // whose lower-right corner is located at (i, j)
         // dp(i, j) = min{ dp(i-1, j-1), dp(i-1, j), dp(i, j-1) }
-        int[][] dp = new int[n + 1][m + 1];
+        int[][] dp = new int[m + 1][n + 1];
 
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
