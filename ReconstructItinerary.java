34debd51f4bb56de744cf53302e7fc51d5e46221
hesunnb <hesun63@gmail.com>
2018-05-30 18:06:17 -0400

    Update ReconstructItinerary.java

--- ReconstructItinerary.java
+++ ReconstructItinerary.java
@@ -22,9 +22,9 @@ class Solution {
     //testCase: 对应discuss的图: 
     //String[][] tickets = {{"JFK","A"},{"JFK","D"},{"A","C"},{"C","D"},{"C","JFK"},{"B","C"},{"D","B"},{"D","A"}};
     
-    /*首先从起点JFK出发, dfs找到一个sub-path: JFK->A->C->D->A, 在A处出现dead end(不再有可以走的边), 此时将A加到解当中, dfs返回. 对于返回到的节点D, 
-    还有可以继续走的subpath, dfs继续找, 得：D->B->C->JFK->D. 此时的D为dead end, 说明可以将D加到解当中, 而且处于已经加过的点之前. 以此类推, 每次都加
-    dead end的节点. 直到所有点都是dead end!*/
+    /*首先从起点JFK出发, dfs找到一个sub-path: JFK->A->C->D->A, 在A处出现dead end(不再有可以走的边), 此时将A加到解当中, dfs返回. 对于返回到的节
+    点D, 还有可以继续走的subpath, dfs继续找, 得：D->B->C->JFK->D. 此时的D为dead end, 说明可以将D加到解当中, 而且处于已经加过的点之前. 以此类推, 
+    每次都加dead end的节点. 直到所有点都是dead end!*/
     
     Map<String, PriorityQueue<String>> flights;
     LinkedList<String> path;
@@ -40,8 +40,8 @@ class Solution {
         return path;
     }
     
-    private void dfs(String departure) { //就是从JFK出发找环, 找到第一个环的dead end就是最后一个点, 然后回退到上一个点继续找环, 遇到的dead end作为
-        //倒数第二个点, 依次进行下去
+    private void dfs(String departure) { //就是从JFK出发找环, 找到第一个环的dead end就是最后一个点, 然后回退到上一个点继续找环, 
+        //遇到的dead end作为倒数第二个点, 依次进行下去
         PriorityQueue<String> arrivals = flights.get(departure);
         while (arrivals != null && !arrivals.isEmpty()) {
             dfs(arrivals.poll());
