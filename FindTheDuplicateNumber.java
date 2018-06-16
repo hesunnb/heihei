87d77fac8a8899931c8b554ee17443b499e77a38
hesunnb <hesun63@gmail.com>
2018-06-16 18:05:37 -0400

    Update FindTheDuplicateNumber.java

--- FindTheDuplicateNumber.java
+++ FindTheDuplicateNumber.java
@@ -17,6 +17,9 @@ Your runtime complexity should be less than O(n2).
 There is only one duplicate number in the array, but it could be repeated more than once.*/
 
 class Solution {
+
+    //solution2: nlog(n)二分解法, 二分搜索基于index和range, 见Kth Smallest Element in a Sorted Matrix, 对于index的已有模板,
+    //那对于这种数组中有重复, unsorted array这种就用这个当模板吧
     public int findDuplicate(int[] nums) {
         
         if(nums == null || nums.length == 0) {
@@ -33,6 +36,14 @@ class Solution {
             if (cnt <= mid) low = mid + 1;
             else high = mid - 1;
         }
-	    return low;
+	return low;
     }
+    /*This solution is based on binary search.
+    At first the search space is numbers between 1 to n. Each time I select a number mid (which is the one in the middle) and count all 
+    the numbers equal to or less than mid. Then if the count is more than mid, the search space will be [1 mid] otherwise [mid+1 n]. I 
+    do this until search space is only one number.
+    Let's say n=10 and I select mid=5. Then I count all the numbers in the array which are less than equal mid. If the there are more 
+    than 5 numbers that are less than 5, then by Pigeonhole Principle (https://en.wikipedia.org/wiki/Pigeonhole_principle) one of them 
+    has occurred more than once. So I shrink the search space from [1 10] to [1 5]. Otherwise the duplicate number is in the second 
+    half so for the next step the search space would be [6 10].*/
 }
