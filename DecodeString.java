e3cced22a73ef0daab3a0200ec1c34867628db2c
hesunnb <hesun63@gmail.com>
2018-07-28 22:01:47 -0400

    Update DecodeString.java

--- DecodeString.java
+++ DecodeString.java
@@ -56,4 +56,34 @@ class Solution {
         }
         return res;
     }
+    
+    
+    //solution2: 递归解法
+    private int pos = 0;
+    public String decodeString(String s) {
+        
+        if(s == null || s.length() == 0) {
+            return "";
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        String num = "";
+        for (int i = pos; i < s.length(); i++) {
+            if (s.charAt(i) != '[' && s.charAt(i) != ']' && !Character.isDigit(s.charAt(i))) {
+                sb.append(s.charAt(i));
+            } else if (Character.isDigit(s.charAt(i))) {
+                num += s.charAt(i);
+            } else if (s.charAt(i) == '[') {
+                pos = i + 1;
+                String next = decodeString(s);
+                for (int n = Integer.valueOf(num); n > 0; n--) sb.append(next);
+                num = "";
+                i = pos;
+            } else if (s.charAt(i) == ']') {
+                pos = i;
+                return sb.toString();
+            }
+        }
+        return sb.toString();
+    }
 }
