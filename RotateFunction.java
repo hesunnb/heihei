744a09595d7cbf7f44cf886845d1ade2b605d9ec
hesunnb <hesun63@gmail.com>
2018-07-30 11:59:45 -0400

    Update RotateFunction.java

--- RotateFunction.java
+++ RotateFunction.java
@@ -22,6 +22,46 @@ So the maximum value of F(0), F(1), F(2), F(3) is F(3) = 26.*/
 
 class Solution {
 
+    //solution1: 数学公式推导法
+    public int maxRotateFunction(int[] A) {
+        
+        if(A == null || A.length == 0) {
+            return 0;
+        }
+        
+        int allSum = 0; //数组元素的和
+        int len = A.length;
+        int F = 0;
+        for (int i = 0; i < len; i++) {
+            F += i * A[i]; //F最开始的值就是F(0)了
+            allSum += A[i];
+        }
+        int max = F;
+        for (int i = len - 1; i >= 1; i--) {
+            F = F + allSum - len * A[i]; //算其他的F()
+            max = Math.max(F, max);
+        }
+        return max;
+    }
+    /*解析: F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-1) * Bk[n-1]
+    F(k-1) = 0 * Bk-1[0] + 1 * Bk-1[1] + ... + (n-1) * Bk-1[n-1]
+           = 0 * Bk[1] + 1 * Bk[2] + ... + (n-2) * Bk[n-1] + (n-1) * Bk[0]
+    Then,
+
+    F(k) - F(k-1) = Bk[1] + Bk[2] + ... + Bk[n-1] + (1-n)Bk[0]
+                  = (Bk[0] + ... + Bk[n-1]) - nBk[0]
+                  = sum - nBk[0]
+    Thus,
+
+    F(k) = F(k-1) + sum - nBk[0]
+    What is Bk[0]?
+
+    k = 0; B[0] = A[0];
+    k = 1; B[0] = A[len-1];
+    k = 2; B[0] = A[len-2];
+    ...*/
+    
+    
     //solution2: (own)
     public int maxRotateFunction(int[] A) {
         
