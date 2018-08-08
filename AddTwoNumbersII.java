221f4225afe832408f54c00a9168f5c5b58faf28
hesunnb <hesun63@gmail.com>
2018-08-08 15:44:42 -0400

    Update AddTwoNumbersII.java

--- AddTwoNumbersII.java
+++ AddTwoNumbersII.java
@@ -21,7 +21,46 @@ Output: 7 -> 8 -> 0 -> 7*/
  */
 class Solution {
     
-    //solution1: follow up里面不让reverse linked list, 所以用discuss的方法
+    //solution1: follow up里面不让reverse linked list, 所以用discuss的方法, 用栈
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        if(l1 == null || l2 == null) {
+            return null;
+        }
+        
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+        
+        while(l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while(l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        
+        ListNode list = new ListNode(0);
+        int carry = 0;
+        while (!stack1.empty() || !stack2.empty()) {
+            if (!stack1.empty()) {
+                carry += stack1.pop(); //这些运算都会带着之前的进位
+            }
+            if (!stack2.empty()) {
+                carry += stack2.pop();
+            }
+            //从尾到头组装一个正序链表的方法, 之前的Add Two Numbers是从头到尾组装一个倒序链表
+            list.val = carry % 10; //list是个位节点; 每次新的个位值都会把之前的十位值给替换掉, 不过没事, 因为之前十位的值保留在carry里, 在上面的求和
+            //过程中已经加进去了
+            ListNode head = new ListNode(carry / 10); //head是十位节点
+            head.next = list; //将十位与个位相连
+            list = head; //个位指针窜到十位
+            carry /= 10; //取出并保留进位
+        }
+        
+        return list.val == 0 ? list.next : list; //最后一次list窜到了head的位置, 那么因为每次new head的时候都用的十位, 所以这里的意思就是
+        //如果最后的十位为0, 那么从list.next开始, 否则就从最后一个十位开始
+    }
     
     
     //solution2: 自己的方法, 运用反转链表
