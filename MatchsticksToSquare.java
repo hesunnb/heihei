146dfc76ea20eaab35523e1ca4e6dc4d1e1e6894
hesunnb <hesun63@gmail.com>
2018-08-21 15:33:26 -0400

    Update MatchsticksToSquare.java

--- MatchsticksToSquare.java
+++ MatchsticksToSquare.java
@@ -20,6 +20,9 @@ The length sum of the given matchsticks is in the range of 0 to 10^9.
 The length of the given matchstick array will not exceed 15.*/
 
 class Solution {
+    /*the partition problem (or number partitioning) is the task of deciding whether a given multiset S of positive integers can be 
+    partitioned into two subsets S1 and S2 such that the sum of the numbers in S1 equals the sum of the numbers in S2. The partition 
+    problem is NP-complete.*/
     
     //This is a NP problem, Time complexity should be O(4^n), n is the length of array, 
     //T(n) = 4 T(n-1) + O(1) = 4^n T(0) + 4^n + 4^(n-1) + ... + 4 + 1 = O(4^n)
