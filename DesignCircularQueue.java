fbe330699817bc0c5c29a05590b7febc02876011
hesunnb <hesun63@gmail.com>
2018-08-27 09:41:21 -0400

    Update DesignCircularQueue.java

--- DesignCircularQueue.java
+++ DesignCircularQueue.java
@@ -99,8 +99,57 @@ class MyCircularQueue {
 
 
 //solution2: 用数组实现
+class MyCircularQueue {
 
-
+    private int[] a;
+    private int front = 0, rear = -1, len = 0;
+    
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    public MyCircularQueue(int k) {
+        a = new int[k];
+    }
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+        if (!isFull()) {
+            rear = (rear + 1) % a.length; //rear起始位置是-1, 对rear和front进行求余来进行循环
+            a[rear] = value;
+            len++;
+            return true;
+        }
+        return false;
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+        if (!isEmpty()) {
+            front = (front + 1) % a.length; //front起始位置是0
+            len--;
+            return true;
+        }
+        return false;
+    }
+    
+    /** Get the front item from the queue. */
+    public int Front() {
+        return isEmpty() ? -1 : a[front];
+    }
+    
+    /** Get the last item from the queue. */
+    public int Rear() {
+        return isEmpty() ? -1 : a[rear];
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() { //通过len来判断满没满
+        return len == 0;
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+        return len == a.length;
+    }
+}
 /**
  * Your MyCircularQueue object will be instantiated and called as such:
  * MyCircularQueue obj = new MyCircularQueue(k);
