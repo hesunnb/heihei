b81a384b175f9dcf4dfc1e66f2d43eb760c9e831
hesunnb <hesun63@gmail.com>
2018-09-10 17:39:55 -0400

    Update MaximumXORofTwoNumbersInAnArray.java

--- MaximumXORofTwoNumbersInAnArray.java
+++ MaximumXORofTwoNumbersInAnArray.java
@@ -1,4 +1,4 @@
-/*Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 231.
+/*Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai < 2^31.
 
 Find the maximum result of ai XOR aj, where 0 ≤ i, j < n.
 
