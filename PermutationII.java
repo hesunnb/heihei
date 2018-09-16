c823bce41d5c88ac9b3ca1c5a1df2db4fac41c30
hesunnb <hesun63@gmail.com>
2018-09-16 10:12:51 -0400

    Update PermutationII.java

--- PermutationII.java
+++ PermutationII.java
@@ -31,18 +31,22 @@ class Solution {
         return result;
     }
     
-    private void permuteUniqueHelper(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> list, ArrayList<Integer> nums, boolean[] visit) {
+    private void permuteUniqueHelper(ArrayList<ArrayList<Integer>> result, ArrayList<Integer> list, ArrayList<Integer> nums, 
+            boolean[] visit) {
         if(list.size() == nums.size()) {
             result.add(new ArrayList(list));
             return;
         }
         
         for(int i = 0; i < nums.size(); i++) {
-            if(i != 0 && nums.get(i) == nums.get(i - 1) && !visit[i - 1]) { //这里的条件意思是i不等于0, //并且i处的值和它前面一位的值相等, 并且i处前一位没有访问过, 这个没有访问过用在回朔的时候, 比如1,1,2,2,2,3 当递归回退到1,1,2的时候, 擦除掉了2, 进而要换成下一个2进行递归, 但是下一个2之前的2已经被标记为没有访问过, 所以后面重复的2都被跳过了
+            if(i != 0 && nums.get(i) == nums.get(i - 1) && !visit[i - 1]) { //这里的条件意思是i不等于0, //并且i处的值和它前面一位的值相等, 
+             //并且i处前一位没有访问过, 这个没有访问过用在回朔的时候, 比如1,1,2,2,2,3 当递归回退到1,1,2的时候, 擦除掉了2, 进而要换成下一个2进行
+             //递归, 但是下一个2之前的2已经被标记为没有访问过, 所以后面重复的2都被跳过了
                 continue;
             }
             
-            if(!visit[i]) { //如果i位置的元素没有被访问过, 就向list里面填入值, 比如1,1,2,2,3,2这种情况, 最后一个2是从头开始遍历1,1,2,2都访问过, 到第三个2没有访问过, 所以加入到list里面
+            if(!visit[i]) { //如果i位置的元素没有被访问过, 就向list里面填入值, 比如1,1,2,2,3,2这种情况, 最后一个2是从头开始遍历1,1,2,2都访问
+            //过, 到第三个2没有访问过, 所以加入到list里面
                 visit[i] = true;
                 list.add(nums.get(i));
                 permuteUniqueHelper(result, list, nums, visit);
