39baa63819621c15ed25c53ae57f44f76e518faa
hesunnb <hesun63@gmail.com>
2018-09-25 13:24:42 -0400

    Update LRUCache.java

--- LRUCache.java
+++ LRUCache.java
@@ -95,3 +95,9 @@ public class LRUCache {
         current.next = tail;
     }
 }
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
