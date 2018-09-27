5fce9d90cf54d180beddba28d84d989f7b17469b
hesunnb <hesun63@gmail.com>
2018-09-27 00:18:50 -0400

    Update LFUCache.java

--- LFUCache.java
+++ LFUCache.java
@@ -97,4 +97,122 @@ class LFUCache {
         lists.get(1).add(key); // 向LinkedHashSet中添加元素
     }
 }
- 
+
+
+//version2: 把LinkedHashSet改为双端链表
+class LFUCache {
+
+    HashMap<Integer, Node> vals; // key, node
+    HashMap<Integer, Integer> counts; // key, count
+    HashMap<Integer, DLList> lists; // count, 双端链表
+    int capacity;
+    int min = -1; // 当capacity满了删除元素的时候用到
+
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        vals = new HashMap<>();
+        counts = new HashMap<>();
+        lists = new HashMap<>();
+        lists.put(1, new DLList()); // 初始化先把count为1的给一个双端链表
+    }
+
+    public int get(int key) {
+        if (capacity <= 0) {
+            return -1;
+        }
+
+        if (!vals.containsKey(key)) {
+            return -1;
+        }
+
+        int count = counts.get(key); // 得到对应的count
+        counts.put(key, count + 1);
+        lists.get(count).remove(vals.get(key)); // 从lists中对应的count中删除, 删除的是node
+        if (count == min && lists.get(count).size() == 0) {
+            min++;
+        }
+
+        if (!lists.containsKey(count + 1)) {
+            lists.put(count + 1, new DLList()); // 没有就新建
+        }
+        lists.get(count + 1).add(vals.get(key)); // 放到下一个更大的+1的count中
+        return vals.get(key).value;
+    }
+
+    public void put(int key, int value) {
+        if (capacity <= 0) {
+            return;
+        }
+        if (vals.containsKey(key)) {
+            vals.get(key).value = value;
+            get(key); // 相同key就调用一下get方法, 调整一下元素的顺序即可
+            return;
+        }
+        if (vals.size() == capacity) {
+            int evit = lists.get(min).removeHead(); //移除头节点就符合了LRUCache的策略
+            vals.remove(evit); //注意removeHead()返回的是node的key, 所以vals移除没有问题, 不能返回node的value
+        }
+        Node node = new Node(key, value);
+        vals.put(key, node);
+        counts.put(key, 1); // count为1
+        min = 1; // 表名count为1的双端链表目前有元素
+        lists.get(1).add(node); // 向双端链表中添加元素
+    }
+    
+    class DLList {
+        private Node head;
+        private Node tail;
+        private int size = 0; //用于做判断用, 判断双端链表有没有空
+        
+        DLList() {
+            head = new Node(-1, -1);
+            tail = new Node(-1, -1);
+            head.next = tail;
+            tail.prev = head;
+        }
+        
+        public void add(Node insert) {
+            moveToTail(insert);
+            size++;
+        }
+        
+        public void remove(Node current) {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+            size--;
+        }
+        
+        public int removeHead() {
+            int result = head.next.key;
+            head.next = head.next.next; //从链表中移除第一个点
+            head.next.prev = head;
+            size--;
+            return result;
+        }
+        
+        public void moveToTail(Node current) { //向链表尾处插入节点
+            current.prev = tail.prev;
+            tail.prev = current;
+            current.prev.next = current;
+            current.next = tail;
+        }
+        
+        public int size() {
+            return this.size;
+        }
+    }
+    
+    class Node { //内部类
+        Node prev;
+        Node next;
+        int key;
+        int value;
+        
+        Node(int key, int value) {
+            this.prev = null;
+            this.next = null;
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
