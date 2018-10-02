1e1fb0c93de114282dff0f5db890686b51d9209a
hesunnb <hesun63@gmail.com>
2018-10-02 13:51:44 -0400

    Update IncreasingSubsequences.java

--- IncreasingSubsequences.java
+++ IncreasingSubsequences.java
@@ -10,7 +10,7 @@ The range of integer in the given array is [-100,100].
 The given array may contain duplicates, and two equal integers should also be considered as a special case of increasing sequence.*/
 
 class Solution {
-    public List<List<Integer>> findSubsequences(int[] nums) {
+	public List<List<Integer>> findSubsequences(int[] nums) {
 
 		List<List<Integer>> result = new ArrayList<>();
 		if (nums == null) {
@@ -22,19 +22,19 @@ class Solution {
 	}
 
 	private void findSubsequenceshelper(List<Integer> list, int index, int[] nums, List<List<Integer>> result) {
-		
+
 		if (list.size() > 1) { // 用list.size() > 1就不用一个变量count计数了
 			result.add(new ArrayList<Integer>(list));
 		}
-		
+
 		Set<Integer> used = new HashSet<>();
 		for (int i = index; i < nums.length; i++) {
 			if (used.contains(nums[i])) {
 				continue;
 			}
 			if (list.size() == 0 || nums[i] >= list.get(list.size() - 1)) {
-				used.add(nums[i]); //used记录的是list一个位置所加入过的值, 这样当list remove掉了这个位置的值的时候, 当再加入新值的时候
-				//如果used当中已经出现过了, 说明这个新值在这个位置上出现过, 已经重复了, 所以要跳过然后继续判断接下来的值
+				used.add(nums[i]); // used记录的是list一个位置所加入过的值, 这样当list remove掉了这个位置的值的时候, 当再加入新值的时候
+				// 如果used当中已经出现过了, 说明这个新值在这个位置上出现过, 已经重复了, 所以要跳过然后继续判断接下来的值
 				list.add(nums[i]);
 				findSubsequenceshelper(list, i + 1, nums, result);
 				list.remove(list.size() - 1);
