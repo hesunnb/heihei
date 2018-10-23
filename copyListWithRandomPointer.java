57ac6c60006b24fc605507474bd6e88d5b141474
hesunnb <hesun63@gmail.com>
2018-10-23 09:44:48 -0400

    Update copyListWithRandomPointer.java

--- copyListWithRandomPointer.java
+++ copyListWithRandomPointer.java
@@ -19,8 +19,8 @@ public class Solution {
      * @return: A new head of a deep copy of the list.
      */
      
-    //next, random没有初始化，默认就是null
-    //第一种方法： 是用哈希表来解决random指针这个问题，存在表中就可以找到啦，普通链表只能next，随意指针不能通过next实现，
+    //next, random没有初始化, 默认就是null
+    //第一种方法: 是用哈希表来解决random指针这个问题, 存在表中就可以找到啦, 普通链表只能next, 随意指针不能通过next实现, 
     //所以就用哈希表
     public RandomListNode copyRandomList(RandomListNode head) {
         // write your code here
@@ -32,7 +32,7 @@ public class Solution {
         RandomListNode dummy = new RandomListNode(0);
         RandomListNode pre = dummy;
         RandomListNode newNode = null;
-        while(head != null) { //每个head节点要判断两次，一个自己在不在，一个自己的random指针指向的结点在不在
+        while(head != null) { //每个head节点要判断两次, 一个自己在不在, 一个自己的random指针指向的结点在不在
             if(map.containsKey(head)) { //判断自己在不在(head作为key)
                 newNode = map.get(head); //在的话就把head对应的value取出来
             }
@@ -47,8 +47,8 @@ public class Solution {
                     newNode.random = map.get(head.random); //存在就把新结点的随意指针指向head随意指针指向的对象
                 }
                 else {
-                    //不存在就建立一个结点，并存入和head随意指针指向结点的值，并建立对应关系；
-                    //因为这个点建立对应关系后就存在与哈希表中，以后找出来就是已经存在点，直接使用
+                    //不存在就建立一个结点, 并存入和head随意指针指向结点的值, 并建立对应关系;
+                    //因为这个点建立对应关系后就存在与哈希表中, 以后找出来就是已经存在点, 直接使用
                     newNode.random = new RandomListNode(head.random.label); 
                     map.put(head.random, newNode.random);
                 }
@@ -59,10 +59,10 @@ public class Solution {
         return dummy.next;
     }
     
-    //第二种方法： 不用哈希表(但是空间仍用啦O(n),并不是O(1))
-    /*第一遍扫的时候巧妙运用next指针， 开始数组是1->2->3->4  。 然后扫描过程中 先建立copy节点 1->1`->2->2`->3->3`->4->4`, 
-    //然后第二遍copy的时候去建立边的copy， 拆分节点, 一边扫描一边拆成两个链表，这里用到两个dummy node。第一个链表变回  1->2->3 , 
-    //然后第二变成 1`->2`->3`  */
+    //第二种方法: 不用哈希表(但是空间仍用啦O(n),并不是O(1))
+    /*第一遍扫的时候巧妙运用next指针, 开始数组是1->2->3->4. 然后扫描过程中, 先建立copy节点 1->1`->2->2`->3->3`->4->4`, 
+    然后第二遍copy的时候去建立边的copy, 拆分节点, 一边扫描一边拆成两个链表, 这里用到两个dummy node. 第一个链表变回 1->2->3, 
+    然后第二变成 1`->2`->3`*/
     public RandomListNode copyRandomList(RandomListNode head) {
         if(head == null) {
             return null;
