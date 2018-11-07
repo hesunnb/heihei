f70f0ed5a5aef38cee7502cb21f69a46de7f25b6
hesunnb <hesun63@gmail.com>
2018-11-07 16:58:10 -0500

    Update minimumPathSum.java

--- minimumPathSum.java
+++ minimumPathSum.java
@@ -16,7 +16,7 @@ Explanation: Because the path 1→3→1→1→1 minimizes the sum.*/
 
 public class Solution {
 
-    //solution1: 二维数组
+    //solution1: O(n^2)空间
     public int minPathSum(int[][] grid) {
         // write your code here
         
@@ -72,5 +72,31 @@ public class Solution {
             }
         }
         return dp[n - 1]; //返回最后一个值
+    }
+  
+  
+    //solution3: O(1)空间优化
+    public int minPathSum(int[][] grid) {
+        
+        if(grid == null || grid.length == 0 || grid[0].length == 0) {
+            return -1;
+        }
+        
+        int m = grid.length;
+        int n = grid[0].length;
+        
+        for(int i = 1; i < m; i++) { //求第一列
+            grid[i][0] = grid[i - 1][0] + grid[i][0];
+        }
+        for(int i = 1; i < n; i++) { //求第一行
+            grid[0][i] = grid[0][i - 1] + grid[0][i];
+        }
+        
+        for(int i = 1; i < m; i++) { //填充其余的空格
+            for(int j = 1; j < n;j++) {
+                grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+            }
+        }
+        return grid[m - 1][n - 1]; //返回最右下角值
     }
 }
