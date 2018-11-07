e091ab4972e3c98a9556fa30f3ac995adc3c4ba1
hesunnb <hesun63@gmail.com>
2018-11-07 17:00:53 -0500

    Update uniquePaths.java

--- uniquePaths.java
+++ uniquePaths.java
@@ -1,8 +1,31 @@
+/*A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid 
+(marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+
+Above is a 7 x 3 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28*/
+
 public class Solution {
-    /**
-     * @param n, m: positive integer (1 <= n ,m <= 100)
-     * @return an integer
-     */
+
     public int uniquePaths(int m, int n) {
         // write your code here
         if (m == 0 || n == 0) {
@@ -17,20 +40,16 @@ public class Solution {
         sum[0][0] = 1;
         
         //初始化
-        for(int i = 1; i < m; i++)
-        {
+        for(int i = 1; i < m; i++) {
             sum[i][0] = 1;
         }
-        for(int i = 1; i < n; i++)
-        {
+        for(int i = 1; i < n; i++) {
             sum[0][i] = 1;
         }
         
         //填补空
-        for(int i = 1; i < m; i++)
-        {
-            for(int j = 1; j < n; j++)
-            {
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
                 sum[i][j] = sum[i - 1][j] + sum[i][j - 1];
             }
         }
