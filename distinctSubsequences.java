fa6ec59f557aff94ad96a956408d0bd8b6f6a0d4
hesunnb <hesun63@gmail.com>
2018-11-12 21:01:10 -0500

    Update distinctSubsequences.java

--- distinctSubsequences.java
+++ distinctSubsequences.java
@@ -52,7 +52,7 @@ public class Solution {
        
         int[][] result = new int[S.length() + 1][T.length() + 1];
         for(int i = 0; i <= S.length(); i++) {
-            result[i][0] = 1; //在s当中挑出t的空串总有一种方案；而第一行默认就是０(j > 0),除了第一个)，意思就是在空串当中挑不出来T当中的内容；
+            result[i][0] = 1; //在s当中挑出t的空串总有一种方案；而第一行默认就是0(j > 0),除了第一个)，意思就是在空串当中挑不出来T当中的内容；
             //这也是比较独特的初始化i和j不一样的一道题
         }
         
