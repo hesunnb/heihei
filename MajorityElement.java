f348562273d15f5fc1ab73a1fd45da8f94e3aaa6
hesunnb <hesun63@gmail.com>
2018-12-26 17:02:55 -0500

    Update MajorityElement.java

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,6 +1,18 @@
 /*Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 
-You may assume that the array is non-empty and the majority element always exist in the array.*/
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2
+
+*/
 
 /*数组不能为空, 且总有次数最多的元素, 重点是超过[n / 2]次, 这就保证了major总是能够换到1上*/
 
@@ -23,14 +35,14 @@ public class Solution {
             return 0; //要确保num有效, 因为这样返回0不是很好
         }
 
-        int major=num[0], count = 1;
-        for(int i=1; i<num.length;i++){
-            if(count==0){ //count = 0的位置说明之前的正好全部抵消完, 从这个位置重新计
+        int major = num[0], count = 1;
+        for(int i = 1; i < num.length; i++) {
+            if(count == 0) { //count = 0的位置说明之前的正好全部抵消完, 从这个位置重新计
                 count++;
-                major=num[i];
-            }else if(major==num[i]){
+                major = num[i];
+            } else if(major == num[i]) {
                 count++;
-            }else count--;
+            } else count--;
 
         }
         return major;
