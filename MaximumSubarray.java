fe511a365814823e8bc6d5295e4aa5ab03435c6b
hesunnb <hesun63@gmail.com>
2018-12-26 23:32:51 -0500

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,12 +1,19 @@
-/*Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
-For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
-the contiguous subarray [4,−1,2,1] has the largest sum = 6.*/
+/*Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
+Example:
 
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
 
+Follow up:
 
-// Dicussion Solution(O(n))
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+
 public class Solution {
+    
+    //greedy: O(n)
     public int maxSubArray(int[] A) {
         if (nums == null || nums.length == 0) {
             return 0;
@@ -18,7 +25,24 @@ public class Solution {
         }
         return maxSoFar;
     }
+    
+    //Prefix Sum: O(n)
+    public int maxSubArray(int[] A) {
+        if (A == null || A.length == 0){
+            return 0;
+        }
+        
+        int max = Integer.MIN_VALUE, sum = 0, minSum = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            max = Math.max(max, sum - minSum);
+            minSum = Math.min(minSum, sum);
+        }
+
+        return max;
+    }
 }
+
 
 //D & C(divide and conquer)
 public class Solution {
@@ -49,7 +73,6 @@ public class Solution {
         return Math.max(leftSub + rightSub, Math.max(left, right));
     }
 }
-
 
 
 //Minimum Subarray:
