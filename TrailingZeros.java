9cfadd5519da6eb69bfe490d03363b9813ca5c31
hesunnb <hesun63@gmail.com>
2018-12-28 13:07:44 -0500

    Update TrailingZeros.java

--- TrailingZeros.java
+++ TrailingZeros.java
@@ -1,6 +1,19 @@
 /*Given an integer n, return the number of trailing zeroes in n!.
 
-Note: Your solution should be in logarithmic time complexity.*/
+Example 1:
+
+Input: 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+
+Example 2:
+
+Input: 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+
+Note: Your solution should be in logarithmic time complexity.
+*/
 
 public class Solution {
     public int trailingZeroes(int n) {
@@ -16,10 +29,10 @@ public class Solution {
     }
     
   
-  // Discussion recursion
-	public int trailingZeroes(int n) {
-	    if(n < 5) return 0;
-	    else return n/5 + trailingZeroes(n/5);
-	}
+    // Discussion recursion
+    public int trailingZeroes(int n) {
+	if(n < 5) return 0;
+	else return n/5 + trailingZeroes(n/5);
+    }
 }
 
