a9266a34c1a314ad9e6e3036a40c682b4adacaed
hesunnb <hesun63@gmail.com>
2018-12-28 13:08:17 -0500

    Update TrailingZeros.java

--- TrailingZeros.java
+++ TrailingZeros.java
@@ -16,15 +16,14 @@ Note: Your solution should be in logarithmic time complexity.
 */
 
 public class Solution {
+	
     public int trailingZeroes(int n) {
         
         int sum = 0;
-        
         while(n > 0) {
             n = n / 5;
             sum += n;
         }
-        
         return sum;
     }
     
