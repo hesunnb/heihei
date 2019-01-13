a2dd8a0e7c53e9d956e35bbbda0c140e74ab11de
hesunnb <hesun63@gmail.com>
2019-01-12 21:25:09 -0500

    Update PartitionArray.java

--- PartitionArray.java
+++ PartitionArray.java
@@ -17,7 +17,34 @@ If nums = [3,2,2,1] and k=2, a valid answer is 1.
 */
 
 public class Solution {
-     
+    
+    //就是快排里面的partition部分, 稍有改动, 见KthLargestElementInAnArray与sortColor2
+    public int partitionArray(int[] nums, int k) {
+        //write your code here
+
+        if(nums == null || nums.length == 0) {
+	    return 0;
+        }
+
+        int store = 0;
+        for(int i = 0; i < nums.length; i++) {
+	    if(nums[i] < k) { //store和i中间卡的都是大于等于k的
+	        swap(nums, store, i);
+	        store++;
+	    }
+        } //在这里不用再交换一次的原因是我以一个固定数为轴, 不是数组的最后一个数, 而原来是int pivot = a[end];
+        //如果传nums.length就会越界
+        return store;
+    }
+    
+    private void swap(int[] nums, int m, int n) {
+        int temp = nums[m];
+        nums[m] = nums[n];
+        nums[n] = temp;
+    }
+    //testcase: 2,1,5,6,2,3	
+
+	
     //双指针法:
     public int partitionArray(int[] nums, int k) {
         if(nums == null || nums.length == 0){
@@ -26,7 +53,6 @@ public class Solution {
         
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-
             while (left <= right && nums[left] < k) { //左指针小于k的时候就往后走, 走到第一个大于k的
                 left++;
             }
@@ -46,31 +72,4 @@ public class Solution {
         }
         return left;
     }
-	
-	
-    //就是快排里面的partition部分
-    public int partitionArray(int[] nums, int k) {
-	    //write your code here
-	    
-	    if(nums == null || nums.length == 0) {
-	        return 0;
-	    }
-	    
-	    int store = 0;
-	    for(int i = 0; i < nums.length; i++) {
-	        if(nums[i] < k) { //store和i中间卡的都是大于等于k的
-	            swap(nums, store, i);
-	            store++;
-	        }
-	    } //在这里不用再交换一次的原因是我以一个固定数为轴, 不是数组的最后一个数, 而原来是int pivot = a[end];
-	    //如果传nums.length就会越界
-	    return store;
-    }
-    
-    private void swap(int[] nums, int m, int n) {
-        int temp = nums[m];
-        nums[m] = nums[n];
-        nums[n] = temp;
-    }
-    //testcase: 2,1,5,6,2,3
 }
