3211bd6265da11064bf272df0966045dc564a629
hesunnb <hesun63@gmail.com>
2019-01-20 13:08:34 -0500

    Update ContinuousSubarraySum.java

--- ContinuousSubarraySum.java
+++ ContinuousSubarraySum.java
@@ -8,12 +8,42 @@ Give [-3, 1, 3, -3, 4], return [1,4].
 */
 
 public class Solution {
-    /**
-     * @param A an integer array
-     * @return  A list of integers includes the index of the first number and the index of the last number
-     */
+
+    //根据前缀和方法改编的方法, MaximumSubarray
+    public List<Integer> continuousSubarraySum(int[] A) {
+        // write your code here
+        List<Integer> result = new ArrayList<>();
+        if (A == null || A.length == 0) {
+            return result;
+        }
+        
+        int max = Integer.MIN_VALUE, sum = 0, minSum = 0;
+        int start = 0;
+        int end = 0;
+        result.add(0);
+        result.add(0);
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            if(sum - minSum > max) { //这里不能有等号, 否则[1,2,-2,-100,1,2,-2]会找到第二个[1,2], 题目说有重复的话要返回第一个[1,2]
+                max = sum - minSum;
+                end = i;
+                result.set(0, start);
+                result.set(1, end);
+            }
+            
+            if(minSum > sum) { //这里也不能加等号, 否则[-2,0,0,1,-1,-1]过不去
+                minSum = sum;
+                start = i + 1; //这里只进行了更新, 没有被set到result当中
+                end = i + 1;
+            }
+        }
+        
+        return result;
+    }   
+ 
+    
+    //贪心改编的方法
     public ArrayList<Integer> continuousSubarraySum(int[] A) {
-        // Write your code here
         
         ArrayList<Integer> result = new ArrayList<Integer>();
         
