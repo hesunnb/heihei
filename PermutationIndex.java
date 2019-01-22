4fc396a8282c1227e90a44782ee0bdc9e174a511
hesunnb <hesun63@gmail.com>
2019-01-21 20:52:09 -0500

    Update PermutationIndex.java

--- PermutationIndex.java
+++ PermutationIndex.java
@@ -68,6 +68,10 @@ public class Solution {
         }
         return sum;
     }
+    /*testCase: 5, 1, 4, 3, 2, 8
+    算式: 4*5! + 0*4! + 2*3! + 1*2! + 0*1! + 1
+    分析: 找5开头前面有多少个, 先找5后面比5小的数的个数(1,4,3,2), 然后乘以(1,4,3,2)每个数作为开头所能组成的个数(就是阶乘), 
+    然后再找以51开头的(1后面有多个比它小的), 然后是514开头的(就是4后面比它小的数的个数), 以此类推*/
 }
 
 
@@ -87,7 +91,7 @@ public class Solution {
     private static long factorial(int n) { //阶乘用long
 	long sum = 1;
 	while(n > 0) {
-		sum *= (n--);
+	    sum *= (n--);
 	}
 	return sum;
     }
@@ -96,15 +100,10 @@ public class Solution {
 	int temp = array[index];
 	int count = 0;
 	for(int i = index; i < array.length; i++) { //从传过来的index开始找index后面比array[index]小的
-		if(array[i] < temp) {
-			count++;
-		}
+	    if(array[i] < temp) {
+		count++;
+	    }
 	}
 	return count;
     }
-	
-    /*testCase: 5, 1, 4, 3, 2, 8
-    算式: 4*5! + 0*4! + 2*3! + 1*2! + 0*1! + 1
-    分析: 找5开头前面有多少个, 先找5后面比5小的数的个数(1,4,3,2), 然后乘以(1,4,3,2)每个数作为开头所能组成的个数(就是阶乘), 
-    然后再找以51开头的(1后面有多个比它小的), 然后是514开头的(就是4后面比它小的数的个数), 以此类推*/
 }
