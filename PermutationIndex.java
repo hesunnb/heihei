a596709c0ebbbc1a2efd1e8ec5f1b7483197e0fe
hesunnb <hesun63@gmail.com>
2019-01-21 20:50:42 -0500

    Update PermutationIndex.java

--- PermutationIndex.java
+++ PermutationIndex.java
@@ -14,6 +14,7 @@ public class Solution {
      
     //用哈希表, O(n^2)时间, O(n)空间
     //详细解释见permutationIndex2
+    //思路: 目的就是求比A中所表示的排列小的排列有多少个
     public long permutationIndex(int[] A) {
         // Write your code here
         
@@ -71,10 +72,7 @@ public class Solution {
 
 
 public class Solution {
-    /**
-     * @param A an integer array
-     * @return a long integer
-     */
+
     public long permutationIndex(int[] A) {
         // Write your code here
         long sum = 1; //这个1就是4*5! + 0*4! + 2*3! + 1*2! + 0*1! + 1中的最后的那个1, 在最开始直接初始化它
@@ -87,27 +85,26 @@ public class Solution {
     }
     
     private static long factorial(int n) { //阶乘用long
-		long sum = 1;
-		while(n > 0) {
-			sum *= (n--);
-		}
-		return sum;
+	long sum = 1;
+	while(n > 0) {
+		sum *= (n--);
 	}
+	return sum;
+    }
 	
-	private static int post(int[] array, int index) {
-		int temp = array[index];
-		int count = 0;
-		for(int i = index; i < array.length; i++) { //从传过来的index开始找index后面比array[index]小的
-			if(array[i] < temp) {
-				count++;
-			}
+    private static int post(int[] array, int index) {
+	int temp = array[index];
+	int count = 0;
+	for(int i = index; i < array.length; i++) { //从传过来的index开始找index后面比array[index]小的
+		if(array[i] < temp) {
+			count++;
 		}
-		return count;
 	}
+	return count;
+    }
 	
-	/*
-	testCase: 5, 1, 4, 3, 2, 8
-	算式: 4*5! + 0*4! + 2*3! + 1*2! + 0*1! + 1
-	分析: 找5开头前面有多少个, 先找5后面比5小的数的个数(1,4,3,2), 然后乘以(1,4,3,2)每个数作为开头所能组成的个数(就是阶乘), 然后再找以51开头的(1后面有多个比它小的), 然后是514开头的(就是4后面比它小的数的个数), 以此类推
-	*/
+    /*testCase: 5, 1, 4, 3, 2, 8
+    算式: 4*5! + 0*4! + 2*3! + 1*2! + 0*1! + 1
+    分析: 找5开头前面有多少个, 先找5后面比5小的数的个数(1,4,3,2), 然后乘以(1,4,3,2)每个数作为开头所能组成的个数(就是阶乘), 
+    然后再找以51开头的(1后面有多个比它小的), 然后是514开头的(就是4后面比它小的数的个数), 以此类推*/
 }
