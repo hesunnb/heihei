3e75cd2b31c47d0cf9335980566a782f1c6789fa
hesunnb <hesun63@gmail.com>
2019-01-29 16:39:51 -0500

    Update UniqueBinarySearchTrees.java

--- UniqueBinarySearchTrees.java
+++ UniqueBinarySearchTrees.java
@@ -38,12 +38,13 @@ public class Solution {
     i.e.
     G(0)=1, G(1)=1. 
 
-    Given a sequence 1…n, we pick a number i out of the sequence as the root, then the number of unique BST with the specified root F(i), 
-    is the cartesian product of the number of BST for its left and right subtrees. For example, F(3, 7): the number of unique BST tree with 
-    number 3 as its root. To construct an unique BST out of the entire sequence [1, 2, 3, 4, 5, 6, 7] with 3 as the root, which is to say, 
-    we need to construct an unique BST out of its left subsequence [1, 2] and another BST out of the right subsequence [4, 5, 6, 7], and 
-    then combine them together (i.e. cartesian product). The tricky part is that we could consider the number of unique BST out of sequence 
-    [1,2] as G(2), and the number of of unique BST out of sequence [4, 5, 6, 7] as G(4). Therefore, F(3,7) = G(2) * G(4).
+    Given a sequence 1…n, we pick a number i out of the sequence as the root, then the number of unique BST with the specified root 
+    F(i), is the cartesian product of the number of BST for its left and right subtrees. For example, F(3, 7): the number of unique 
+    BST tree with number 3 as its root. To construct an unique BST out of the entire sequence [1, 2, 3, 4, 5, 6, 7] with 3 as the 
+    root, which is to say, we need to construct an unique BST out of its left subsequence [1, 2] and another BST out of the right 
+    subsequence [4, 5, 6, 7], and then combine them together (i.e. cartesian product). The tricky part is that we could consider 
+    the number of unique BST out of sequence [1,2] as G(2), and the number of of unique BST out of sequence [4, 5, 6, 7] as G(4). 
+    Therefore, F(3,7) = G(2) * G(4).
     i.e.
     F(i, n) = G(i-1) * G(n-i)	1 <= i <= n 
 
