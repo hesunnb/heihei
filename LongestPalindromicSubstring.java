1e1c293b2f86a26b135eb545188034ed9c57509c
hesunnb <hesun63@gmail.com>
2019-02-19 13:59:24 -0500

    Update LongestPalindromicSubstring.java

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -14,6 +14,39 @@ Input: "cbbd"
 Output: "bb"*/
 
 public class Solution {
+    
+    //目前最流行的写法:
+    String longest = "";
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() == 0) {
+            return longest;
+        }
+        
+        for(int i = 0; i < s.length(); i++){
+            //计算奇数子字符串
+            helper(s, i, 0);
+            //计算偶数子字符串
+            helper(s, i, 1);
+        }
+        return longest;
+    }
+    
+    private void helper(String s, int idx, int offset) {
+        int left = idx;
+        int right = idx + offset;
+        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)) {
+            left--;
+            right++;
+        }
+        // 截出当前最长的子串
+        String currLongest = s.substring(left + 1, right);
+        // 判断是否比全局最长还长
+        if(currLongest.length() > longest.length()){
+            longest = currLongest;
+        }
+    }
+    
+    
     public String longestPalindrome(String s) {
         if(s == null || s.length() == 0) {
             return null;
