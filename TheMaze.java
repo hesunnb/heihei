e314b27965ebfe1420c038ae4950d2108e25d1c8
hesunnb <hesun63@gmail.com>
2019-02-22 14:44:49 -0500

    Update TheMaze.java

--- TheMaze.java
+++ TheMaze.java
@@ -28,13 +28,56 @@ maze are all walls.
 5.The maze contains at least 2 empty spaces, and both the width and height of the maze won't exceed 100.*/
 
 public class Solution {
-    /**
-     * @param maze: the maze
-     * @param start: the start
-     * @param destination: the destination
-     * @return: whether the ball could stop at the destination
-     */
-     
+
+    //boolean[][] visited也可以没有, 省空间, 用其他值替代一下就可以了, 比如这里用2来代表在此处撞过墙
+    int[] dx = new int[]{-1,1,0,0};
+    int[] dy = new int[]{0,0,-1,1};
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        // write your code here
+        
+        if(maze == null || maze.length == 0 || maze[0].length == 0 || start == null || start.length == 0 || destination == null || 
+           destination.length == 0) {
+            return false;
+        }
+        
+        return hasPathHelper(maze, start[0], start[1], destination);
+    }
+    
+    public boolean hasPathHelper(int[][] maze, int x, int y, int[] destination) {
+        if(!isValid(maze, x, y) || maze[x][y] == 2) {
+            return false;
+        }
+        
+        if(x == destination[0] && y == destination[1]) { //还有就是这道题最终判断的是小球能不能停在destination, 
+            //也就是小球在destination处一定是撞墙状态, 路过destination不算数的
+            return true;
+        }
+        
+        maze[x][y] = 2;
+        for(int i = 0; i < 4; i++) { //4个方向, 只有撞墙了才能改变方向, 途中不能改变方向
+            int xx = x;
+            int yy = y;
+            while(isValid(maze, xx + dx[i], yy + dy[i])) { //重点是这里, 向某一个方向走不停的处理
+                xx += dx[i];
+                yy += dy[i]; //isValid和visited是不同的东西, 只有撞墙停下了才会去考虑visited, 向某一个方向走不停的时候
+                //不会考虑visited, 这个一定注意
+            }
+            if(hasPathHelper(maze, xx, yy, destination)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean isValid(int[][] maze, int x, int y) {
+        if(x < 0 || y < 0 || x >= maze.length || y >= maze[0].length || maze[x][y] == 1) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    //用boolean[][] visited数组
     int[] dx = new int[] {-1,1,0,0};
     int[] dy = new int[] {0,0,-1,1};
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
