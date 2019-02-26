15eb70c8a8dd2d7fa8a8d1379c250ff832ece900
hesunnb <hesun63@gmail.com>
2019-02-26 16:17:46 -0500

    Update Add Binary.java

--- Add Binary.java
+++ Add Binary.java
@@ -1,35 +1,23 @@
 public class Solution {
-    /**
-     * @param a a number
-     * @param b a number
-     * @return the result
-     */
      
     //思路就是模拟二进制的加法运算，求余得结果，求商得进位
     public String addBinary(String a, String b) {
         // Write your code here
-        
-        
-        
         //version 1: 1个while
-        if(a == null || b == null)
-        {
+        if(a == null || b == null) {
             return null;
         }
         
-        int pa = a.length() - 1;
-        int pb = b.length() - 1;
+        int aLen = a.length() - 1;
+        int bLen = b.length() - 1;
         StringBuilder sb = new StringBuilder();
         int carries = 0;
-        while(pa >= 0 || pb >= 0 || carries == 1) //把最后的carries是不是1的判断也合并到这里来, 就只剩一个while啦
-        {
+        while(aLen >= 0 || bLen >= 0 || carries == 1) { //把最后的carries是不是1的判断也合并到这里来, 就只剩一个while啦
             int sum = carries;
-            if(pa >= 0)
-            {
+            if(aLen >= 0) {
                 sum += a.charAt(pa--) - '0';
             }
-            if(pb >= 0)
-            {
+            if(bLen >= 0) {
                 sum += b.charAt(pb--) - '0';
             }
             sb.insert(0, sum % 2); //这里用insert在头部插入就行
@@ -38,11 +26,7 @@ public class Solution {
         
         return sb.toString();
         //如果要是10进制以内, 就加入一个k代表进制, 然后每次sum % k, sum / k就行啦
-        
-        
-        
-        
-        
+
         
         //version 2: 1个while还有一个if
         if(a == null || b == null)
@@ -76,12 +60,7 @@ public class Solution {
         
         return sb.toString();
         
-        
-        
-        
-        
-        
-        
+
         //version 3: 2个while
         /*if(a.length() < b.length()) //这里确保a最大，做和，谁最大都行
         {
@@ -99,7 +78,8 @@ public class Solution {
         {
             //一定要减'0'，不减'0'的话，就成啦'0'或'1'的ASCII值啦
             int sum = (int)(a.charAt(pa) - '0') + (int)(b.charAt(pb) - '0') + carries; //求和带进位
-            result = String.valueOf(sum % 2) + result; //把每次得到的余数加到结果里,不能写成+=，因为要把每次的结果加到前面，不是顺次加，所以要把得到的余数写在前，result写在后
+            result = String.valueOf(sum % 2) + result; //把每次得到的余数加到结果里,不能写成+=，因为要把每次的结果加到前面，不是顺次加，
+            //所以要把得到的余数写在前，result写在后
             carries = sum / 2; //得到进位
             pa--;
             pb--;
