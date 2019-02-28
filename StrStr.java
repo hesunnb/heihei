71307305133549d8c6615b40718db8ef8958b969
hesunnb <hesun63@gmail.com>
2019-02-27 21:14:40 -0500

    Update StrStr.java

--- StrStr.java
+++ StrStr.java
@@ -1,7 +1,14 @@
 /*Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.*/
 /*For a given source string and a target string, you should output the first index(from 0) of target string in source string.
 
-If target does not exist in source, just return -1.*/
+If target does not exist in source, just return -1.
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's 
+indexOf().*/
 
 public class Solution {
     public int strStr(String haystack, String needle) {
