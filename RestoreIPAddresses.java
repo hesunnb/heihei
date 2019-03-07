e0a4d99adc56a16e61847d4024cdea2eb2ee4ba6
hesunnb <hesun63@gmail.com>
2019-03-07 09:11:39 -0500

    Update RestoreIPAddresses.java

--- RestoreIPAddresses.java
+++ RestoreIPAddresses.java
@@ -59,45 +59,48 @@ Expected: ["0.10.0.10","0.100.1.0"] //处理之后
 
 //DFS
 public class Solution {
-    public ArrayList<String> restoreIpAddresses(String s) {
-        ArrayList<String> result = new ArrayList<String>();
-        ArrayList<String> list = new ArrayList<String>();
+    public List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<String>();
+        List<String> list = new ArrayList<String>();
         
-        if(s.length() <4 || s.length() > 12)
+        if(s.length() < 4 || s.length() > 12) {
             return result;
+        }
         
-        helper(result, list, s , 0);
+        helper(result, list, s, 0);
         return result;
     }
     
-    public void helper(ArrayList<String> result, ArrayList<String> list, String s, int start){
-        if(list.size() == 4){
-            if(start != s.length())
+    public void helper(List<String> result, List<String> list, String s, int start) {
+        if(list.size() == 4) {
+            if(start != s.length()) {
                 return;
-            
-            StringBuffer sb = new StringBuffer();
-            for(String tmp: list){
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for(String tmp : list){
                 sb.append(tmp);
                 sb.append(".");
             }
-            sb.deleteCharAt(sb.length()-1);
+            sb.deleteCharAt(sb.length() - 1);
             result.add(sb.toString());
             return;
         }
         
-        for(int i=start; i<s.length() && i < start+3; i++){
-            String tmp = s.substring(start, i+1);
+        for(int i = start; i < s.length() && i < start + 3; i++) {
+            String tmp = s.substring(start, i + 1);
             if(isvalid(tmp)){
                 list.add(tmp);
-                helper(result, list, s, i+1);
-                list.remove(list.size()-1);
+                helper(result, list, s, i + 1);
+                list.remove(list.size() - 1);
             }
         }
     }
     
     private boolean isvalid(String s){
-        if(s.charAt(0) == '0')
+        if(s.charAt(0) == '0') {
             return s.equals("0"); // to eliminate cases like "00", "10"
+        }
         int digit = Integer.valueOf(s);
         return digit >= 0 && digit <= 255;
     }
