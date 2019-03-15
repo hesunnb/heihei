07d7db38b63bca774ab055e8ec1e45f19f6f06d9
hesunnb <hesun63@gmail.com>
2019-03-15 16:36:09 -0400

    Update NextGreaterElementII.java

--- NextGreaterElementII.java
+++ NextGreaterElementII.java
@@ -12,8 +12,28 @@ Note: The length of given array won't exceed 10000.*/
 
 class Solution {
 
-    
+    //值得学习的方法
+    public int[] nextGreaterElements(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return nums;
+        }
+        
+        int[] result = new int[nums.length];
+        Arrays.fill(result, -1);
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < nums.length * 2; i++) {
+            int index = i % nums.length; //循环用求余解决
+            while(!stack.isEmpty() && nums[stack.peek()] < nums[index]) {
+                result[stack.pop()] = nums[index];
+            }
+            if(index < nums.length) {
+                stack.push(index); //放入栈中的是下标
+            }
+        }
+        return result;
+    }
 
+    
     //(own), 思路想法和discuss一样, 但是discuss写的更好
     public int[] nextGreaterElements(int[] nums) {
         if(nums == null || nums.length == 0) {
@@ -21,12 +41,12 @@ class Solution {
         }
         
         int[] result = new int[nums.length];
-        for(int i = 0; i < result.length; i++) {
+        for(int i = 0; i < result.length; i++) { //可以用Arrays.fill替换
             result[i] = -1;
         }
         Stack<Point> stack = new Stack<>();
         List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < 2; i++) {
+        for(int i = 0; i < 2; i++) { //循环用求余可以解决
             for(int j = 0; j < nums.length; j++) {
                 list.add(nums[j]);
             }
@@ -38,7 +58,7 @@ class Solution {
                 result[p.index] = list.get(i);
             }
             if(i < nums.length) {
-                stack.push(new Point(list.get(i), i));
+                stack.push(new Point(list.get(i), i)); //放入栈可以放入下标
             }
         }
         return result;
