254c848878a0380569b7b7f4e80f92eef5267f68
hesunnb <hesun63@gmail.com>
2019-03-26 23:19:24 -0400

    Update ContainerWithMostWater.java

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -1,5 +1,5 @@
-/*Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such that 
-the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the 
+/*Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). n vertical lines are drawn such 
+that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the 
 container contains the most water.
 
 Note: You may not slant the container and n is at least 2.
@@ -28,4 +28,9 @@ class Solution {
         }
         return maxArea;
     }
+    /*left与right谁短移动谁的原因:
+    按照题目所给图分析: 因为计算矩形面积是按短边算, 所以如果向内移动长边, 即使遇到更长的边, 计算面积的时候还是会按没有移动的短边算, 并且同时x轴
+    的间距也变短了, 所以向内移动长边的结果就是面积一定会变小;
+    相反如果向内移动短边, x轴的间距变小了, 但是如果遇到更长的边, 计算面积的时候就会按没有移动的长边计算, 这样面积就有可能变大, 所以结果就是
+    谁短移动谁*/
 }
