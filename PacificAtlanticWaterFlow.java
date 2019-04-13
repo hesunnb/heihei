802ad13f50fe9d89ae1fbb986c7563d93cd9f872
hesunnb <hesun63@gmail.com>
2019-04-13 16:53:20 -0400

    Update PacificAtlanticWaterFlow.java

--- PacificAtlanticWaterFlow.java
+++ PacificAtlanticWaterFlow.java
@@ -25,9 +25,11 @@ Return:
 [[0, 4], [1, 3], [1, 4], [2, 2], [3, 0], [3, 1], [4, 0]] (positions with parentheses in above matrix).*/
 
 class Solution {
+	
+    //以后都要用这种dir的方式来缩短代码长度
+    //(own), 最普通的dfs
     int[][] dir = new int[][] {{-1,0},{1,0},{0,-1},{0,1}};
     public List<int[]> pacificAtlantic(int[][] matrix) {
-        
         List<int[]> result = new ArrayList<>();
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return result;
@@ -38,7 +40,7 @@ class Solution {
             for(int j = 0; j < matrix[0].length; j++) {
                 if(helper(new boolean[1], new boolean[1], i, j, matrix, canReach)) {
                     canReach[i][j] = true;
-                    result.add(new int[]{i, j});
+                    result.add(new int[] {i, j});
                 }
             }
         }
@@ -46,13 +48,14 @@ class Solution {
     }
     
     public boolean helper(boolean[] left, boolean[] right, int x, int y, int[][] matrix, boolean[][] canReach) {
-	if(canReach[x][y]) {
+	if(canReach[x][y]) { //canReach就是记忆化搜索, 如果某个位置之前能够到达两边, 那么就记录, 再次到这个点的时候就不用再向下递归了
             return true;
         }
-        if(x == 0 || y == 0) {
-            left[0] = true;
+        if(x == 0 || y == 0) { //能够到达左边, 左右用数组的原因是能够记录下之前是否到达过左右两边, 而不受递归传值的影响
+            left[0] = true; //值传递的问题就是最开始的点初始化是false, false, 当递归回到这个初始点继续向别的方向走的时候就会重新传入
+	    //false, false, 而把之前得到的true的结果给丢掉了
         }
-        if(x == matrix.length - 1 || y == matrix[0].length - 1) {
+        if(x == matrix.length - 1 || y == matrix[0].length - 1) { //能够到达右边
             right[0] = true;
         }
         if(left[0] && right[0]) {
