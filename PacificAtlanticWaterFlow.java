9c1b71f7c3402e225c3caa13788f241fbed1c787
hesunnb <hesun63@gmail.com>
2019-04-13 19:50:07 -0400

    Update PacificAtlanticWaterFlow.java

--- PacificAtlanticWaterFlow.java
+++ PacificAtlanticWaterFlow.java
@@ -77,4 +77,45 @@ class Solution {
         matrix[x][y] = tmp;
         return false;
     }
+	
+    
+    //dfs discuss: 反其道而行之, 因为把左右边分开了, 每个矩阵负责一边, 所以速度会更快
+    int[][]dir = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    public List<int[]> pacificAtlantic(int[][] matrix) {
+        List<int[]> res = new LinkedList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return res;
+        }
+        int n = matrix.length, m = matrix[0].length;
+        boolean[][]pacific = new boolean[n][m];
+        boolean[][]atlantic = new boolean[n][m];
+        for(int i = 0; i < n; i++) {
+            dfs(matrix, pacific, Integer.MIN_VALUE, i, 0); //从边上能到达的低点出发向中间的高点走, 途中标记能到达的点
+            dfs(matrix, atlantic, Integer.MIN_VALUE, i, m-1);
+        }
+        for(int i = 0; i < m; i++) {
+            dfs(matrix, pacific, Integer.MIN_VALUE, 0, i);
+            dfs(matrix, atlantic, Integer.MIN_VALUE, n-1, i);
+        }
+        for (int i = 0; i < n; i++) { //两个矩阵综合就是都能到达的点
+            for (int j = 0; j < m; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    res.add(new int[] {i, j});
+                }   
+            }      
+        }
+            
+        return res;
+    }
+    
+    public void dfs(int[][]matrix, boolean[][]visited, int height, int x, int y){
+        int n = matrix.length, m = matrix[0].length;
+        if(x<0 || x>=n || y<0 || y>=m || visited[x][y] || matrix[x][y] < height) {
+            return;
+        }
+        visited[x][y] = true;
+        for(int[]d : dir){
+            dfs(matrix, visited, matrix[x][y], x+d[0], y+d[1]);
+        }
+    } 
 }
