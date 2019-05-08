8cdc522aa9ccf78a9f1ec49d9e2a81cd80a676f2
hesunnb <hesun63@gmail.com>
2019-05-07 23:29:22 -0400

    Update wordBreakII.java

--- wordBreakII.java
+++ wordBreakII.java
@@ -1,6 +1,12 @@
-/*{dog=[dog]}
+/*
+{dog=[dog]}
 {sanddog=[sand dog], dog=[dog]}
 {anddog=[and dog], sanddog=[sand dog], dog=[dog]}
 {anddog=[and dog], sanddog=[sand dog], dog=[dog], catsanddog=[cat sand dog, cats and dog]}
 
+{apple=[apple]}
+{apple=[apple], penapple=[pen apple]}
+{apple=[apple], penapple=[pen apple], applepenapple=[apple pen apple, applepen apple]}
+{apple=[apple], pineapplepenapple=[pine apple pen apple, pine applepen apple, pineapple pen apple], penapple=[pen apple], 
+applepenapple=[apple pen apple, applepen apple]}
 */
