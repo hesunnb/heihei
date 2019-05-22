5197f57503348240d349e1fe4b1e826858653f64
hesunnb <hesun63@gmail.com>
2019-05-21 22:57:05 -0400

    Update houseRobber.java

--- houseRobber.java
+++ houseRobber.java
@@ -14,6 +14,24 @@ Given [3, 8, 4], return 8.
 public class Solution {
     
     //状态方程: dp[i] = max(dp[i-1], dp[i-2] + A[i-1])
+    //O(n)空间
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int[] result = new int[nums.length + 1];
+        result[0] = 0;
+        result[1] = nums[0];
+        
+        for(int i = 2; i <= nums.length; i++) {
+            result[i] = Math.max(result[i - 1], result[i - 2] + nums[i - 1]);
+        }
+        
+        return result[nums.length];
+    }
+    
+    
     public int rob(int[] nums) {
 
         if(nums == null || nums.length == 0) {
