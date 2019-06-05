997c218214b6d908f625220b53b4720faf70fc90
hesunnb <hesun63@gmail.com>
2019-06-05 17:57:12 -0400

    Update SerializeAndDeserializeBST.java

--- SerializeAndDeserializeBST.java
+++ SerializeAndDeserializeBST.java
@@ -27,13 +27,67 @@ public class Codec {
     
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        
+        if(root == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        serializeHelper(root, sb);
+        return sb.toString();
+    }
+    
+    //前序遍历装树
+    private void serializeHelper(TreeNode root, StringBuilder sb) {
+        if(root == null) {
+            return;
+        }
+        sb.append(root.val + ",");
+        serializeHelper(root.left, sb);
+        serializeHelper(root.right, sb);
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        
+        if(data == null || data.length() == 0) {
+            return null;
+        }
+        String[] dataArray = data.split(","); //serialize完末尾会有一个",", 但是split对于末尾的分隔符忽略, 对于前面的分隔符会产生"".
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < dataArray.length; i++) {
+            queue.offer(Integer.parseInt(dataArray[i]));
+        }
+        return deserializeHelper(queue);
     }
+    
+    //分隔处变为左边树一个队列, 右边树一个队列
+    private TreeNode deserializeHelper(Queue<Integer> queue) {
+        if(queue.isEmpty()) {
+            return null;
+        }
+        TreeNode root = new TreeNode(queue.poll());
+        Queue<Integer> leftQueue = new LinkedList<>();
+        while(!queue.isEmpty() && queue.peek() < root.val) { //比root小的为左树, 装到leftQueue里
+            leftQueue.offer(queue.poll());
+        }
+        root.left = deserializeHelper(leftQueue); //左树队列
+        root.right = deserializeHelper(queue); //左树都弹出来, queue剩下的就是右树队列
+        return root;
+    }
+    /*Hi all, I think my solution is pretty straightforward and easy to understand, not that efficient though. And the serialized 
+    tree is compact.
+    Pre order traversal of BST will output root node first, then left children, then right.
+
+    root left1 left2 leftX right1 rightX
+    If we look at the value of the pre-order traversal we get this:
+
+    rootValue (<rootValue) (<rootValue) (<rootValue) |separate line| (>rootValue) (>rootValue)
+    Because of BST's property: before the |separate line| all the node values are less than root value, all the node values 
+    after |separate line| are greater than root value. We will utilize this to build left and right tree.
+
+    Pre-order traversal is BST's serialized string. I am doing it iteratively.
+    To deserialized it, use a queue to recursively get root node, left subtree and right subtree.
+
+    I think time complexity is O(NlogN).
+    Errr, my bad, as @ray050899 put below, worst case complexity should be O(N^2), when the tree is really unbalanced.*/
 }
 
 // Your Codec object will be instantiated and called as such:
