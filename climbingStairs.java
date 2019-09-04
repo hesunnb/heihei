358907a7e94def49f109c62081c63defb70897e9
hesunnb <hesun63@gmail.com>
2019-09-03 20:35:23 -0400

    Update and rename climbing stairs to climbingStairs.java

--- climbingStairs.java
+++ climbingStairs.java
@@ -1,22 +1,18 @@
 public class Solution {
-    /**
-     * @param n: An integer
-     * @return: An integer
-     */
+
+    //n步就是n个台阶，就是斐波那契数列
+    /* 0 1 2 3 4...
+       1 1 2 3 5...*/
     public int climbStairs(int n) {
-        // write your code here
-        //n步就是n个台阶，就是斐波那契数列
-        
-        if(n <= 1)
-        {
+
+        if(n <= 1) {
             return 1;
         }
         
         int last = 1;
         int lastlast = 1;
         int now = 0;
-        for(int i = 2; i <= n; i++)
-        {
+        for(int i = 2; i <= n; i++) {
             now = last + lastlast;
             lastlast = last;
             last = now;
@@ -49,36 +45,33 @@ public class Solution {
     
     public static int shishi(int n) {
 		
-		//2步与3步
-		int lll = 0; //人为写出头三项
-		int ll = 1;
-		int l = 1;
-		int now = 0;
-		if(n == 1) {
-			return lll;
-		} else if(n == 2) {
-			return ll;
-		} else if(n == 3) {
-			return l;
-		}
-		
-		for(int i = 4; i <= n; i++) { //公式
-			now = lll + ll;
-			lll = ll;
-			ll = l;
-			l = now;
-		}
-		return now;
+	//2步与3步
+	int lll = 0; //人为写出头三项
+	int ll = 1;
+	int l = 1;
+	int now = 0;
+	if(n == 1) {
+	    return lll;
+	} else if(n == 2) {
+	    return ll;
+	} else if(n == 3) {
+	    return l;
 	}
+
+	for(int i = 4; i <= n; i++) { //公式
+	    now = lll + ll;
+	    lll = ll;
+	    ll = l;
+	    l = now;
+	}
+	return now;
+    }
     
-    /*//递归会超时，每次都把之前的所有的都从新算了一遍
-    public int climbStairs(int n)
-    {
-        if(n <= 1)
-        {
+    /* //递归会超时，每次都把之前的所有的都从新算了一遍
+    public int climbStairs(int n) {
+        if(n <= 1) {
             return 1;
         }
         return climbStairs(n - 1) + climbStairs(n - 2);
-    }*/
-    
+    } */
 }
