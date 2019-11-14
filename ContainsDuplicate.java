d9888d0a4f4a864b58ceb56d0c215a458e7abbe9
hesunnb <hesun63@gmail.com>
2019-11-14 16:16:56 -0500

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -2,6 +2,33 @@
 appears at least twice in the array, and it should return false if every element is distinct.*/
 
 class Solution {
+	
+    //brute force, Time complexity: O(N^2), memory: O(1)
+	public boolean containsDuplicate(int[] nums) {
+
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    //sort and check, Time complexity: O(N lg N), memory: O(1) - not counting the memory used by sort
+    public boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+        for(int ind = 1; ind < nums.length; ind++) {
+            if(nums[ind] == nums[ind - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    //hashtable, Time complexity: O(N), memory: O(N)
     public boolean containsDuplicate(int[] nums) {
         if(nums == null || nums.length == 0) {
             return false;
@@ -14,6 +41,6 @@ class Solution {
             }
         }
 			
-	return false;
+	    return false;
     }
 }
