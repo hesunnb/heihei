ab5004e9cd150f92e273078c0c1605c3f96324a1
hesunnb <hesun63@gmail.com>
2019-11-16 18:42:40 -0500

    Update IntegerToEnglishWords.java

--- IntegerToEnglishWords.java
+++ IntegerToEnglishWords.java
@@ -19,51 +19,6 @@ Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Th
 
 class Solution {
     
-    //递归
-    private String[] LESS_THAN_20 = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
-            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
-            "Nineteen" };
-    private String[] TENS = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty",
-            "Ninety" }; //注意这里开头有两个""
-    private String[] THOUSANDS = { "", "Thousand", "Million", "Billion" };
-
-    public String numberToWords(int num) {
-        if (num == 0) {
-            return "Zero";
-        }
-
-        int i = 0;
-        String words = "";
-
-        while (num > 0) {
-            if (num % 1000 != 0) {
-                words = helper(num % 1000) + " " + THOUSANDS[i] + " " + words;
-            }
-            num /= 1000;
-            i++;
-        }
-
-        return words.trim();
-    }
-
-    private String helper(int num) {
-        if (num == 0) {
-            return "";
-        }
-        else if (num < 20) {
-            return LESS_THAN_20[num];
-        }
-        else if (num < 100) {
-            return TENS[num / 10] + " " + helper(num % 10);
-        }
-        else {
-            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
-        }
-    }
-}
-
-class Solution {
-    
     //循环
     private String[] LESS_THAN_20 = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
             "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
@@ -110,5 +65,50 @@ class Solution {
         }
         
         return sb.toString().trim();
+    }
+}
+
+class Solution {
+    
+    //递归
+    private String[] LESS_THAN_20 = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
+            "Nineteen" };
+    private String[] TENS = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty",
+            "Ninety" }; //注意这里开头有两个""
+    private String[] THOUSANDS = { "", "Thousand", "Million", "Billion" };
+
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+
+        int i = 0;
+        String words = "";
+
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+            }
+            num /= 1000;
+            i++;
+        }
+
+        return words.trim();
+    }
+
+    private String helper(int num) {
+        if (num == 0) {
+            return "";
+        }
+        else if (num < 20) {
+            return LESS_THAN_20[num] + " ";
+        }
+        else if (num < 100) {
+            return TENS[num / 10] + " " + helper(num % 10);
+        }
+        else {
+            return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+        }
     }
 }
