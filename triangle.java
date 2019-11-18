5a2cc9cdee7c0963b3586c172b241b9a8da0bdce
hesunnb <hesun63@gmail.com>
2019-11-18 14:48:36 -0500

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -30,6 +30,23 @@ public class Solution {
         */
     }
     
+    //接口是list
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle == null || triangle.size() == 0 || triangle.get(0).size() == 0) {
+            return -1;
+        }
+        
+        int n = triangle.size();
+        for(int i = n - 1; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                triangle.get(i - 1).set(j, triangle.get(i - 1).get(j) + Math.min(triangle.get(i).get(j), triangle.get(i).get(j + 1))); 
+                //从底向上
+                //两两与上面的做和然后替换掉上面的数
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+    
     
     //开了O(n)的空间, 时间O(n2)
     public int minimumTotal(int[][] triangle) {
