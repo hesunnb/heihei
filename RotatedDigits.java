3fb8561b2b4a9f65a7c532dcac2ae016f693fa6a
hesunnb <hesun63@gmail.com>
2020-01-01 13:30:07 -0500

    Update RotatedDigits.java

--- RotatedDigits.java
+++ RotatedDigits.java
@@ -1,8 +1,8 @@
-/*X is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from X.  
-Each digit must be rotated - we cannot choose to leave it alone.
+/*X is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from 
+X. Each digit must be rotated - we cannot choose to leave it alone.
 
-A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other; 
-6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
+A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each 
+other; 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
 
 Now given a positive number N, how many numbers X from 1 to N are good?
 
@@ -18,7 +18,7 @@ N  will be in range [1, 10000].*/
 
 class Solution {
 
-    //自己写的把N转成String判断包含至少一个2, 5, 6, 9, 不包含3, 4 or 7也能过, 就是慢了, 所以直接用求余
+    //自己写的把N转成String再判断包含至少一个2, 5, 6, 9, 不包含3, 4 or 7也能过, 就是慢了, 所以直接用求余
     public int rotatedDigits(int N) {
         if(N <= 0) {
             return 0;
