fc20723412dddf4d50f196da9d9da7eff0bdc581
hesunnb <hesun63@gmail.com>
2020-01-15 11:58:11 -0500

    Create jumpGame3.java

---
+++ jumpGame3.java
@@ -0,0 +1,68 @@
+/*Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, 
+you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+
+Notice that you can not jump outside of the array at any time.
+
+ 
+
+Example 1:
+
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation: 
+All possible ways to reach at index 3 with value 0 are: 
+index 5 -> index 4 -> index 1 -> index 3 
+index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+Example 2:
+
+Input: arr = [4,2,3,0,3,1,2], start = 0
+Output: true 
+Explanation: 
+One possible way to reach at index 3 with value 0 is: 
+index 0 -> index 4 -> index 1 -> index 3
+Example 3:
+
+Input: arr = [3,0,2,1,2], start = 2
+Output: false
+Explanation: There is no way to reach at index 1 with value 0.
+ 
+
+Constraints:
+
+1 <= arr.length <= 5 * 10^4
+0 <= arr[i] < arr.length
+0 <= start < arr.length*/
+
+class Solution {
+
+    //dfs, 思路就是从start出发, 对每个点尝试左右走, 遍历所有点就ok
+    public boolean canReach(int[] arr, int start) {
+        if(arr == null || arr.length == 0 || start < 0 || start > arr.length - 1) {
+            return false;
+        }
+        
+        return helper(arr, start, new boolean[arr.length]);
+    }
+    
+    //result用来记录哪些点访问过了, 访问过的点再访问到就要返回, 否则[3,0,2,1,2]这种无法到达元素0的例子就会死循环
+    private boolean helper(int[] arr, int start, boolean[] result) { 
+        if(start < 0 || start > arr.length - 1) {
+            return false;
+        }
+        if(result[start]) { //访问过了就返回
+            return false;
+        }
+        if(arr[start] == 0) {
+            return true;
+        }
+        
+        result[start] = true; //此处标识该点访问过了
+        if(helper(arr, start - arr[start], result)) {
+            return true;
+        }
+        if(helper(arr, start + arr[start], result)) {
+            return true;
+        }
+        return false;
+    }
+}
