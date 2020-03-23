33167bd73ceeebc1b3cadde5b5a7cdef6a2d12c4
hesunnb <hesun63@gmail.com>
2020-03-23 11:21:25 -0400

    Update mergeKSortedLists.java

--- mergeKSortedLists.java
+++ mergeKSortedLists.java
@@ -17,7 +17,7 @@ public class Solution {
      * @return: The head of one sorted list.
      */
      
-//方法1：
+    //方法1：
     //好: 两两merge(1和2,3和4,5和6...), 然后把结果再两两merge, 最后成为一个链表, 复杂度还是knlogn(自下而上), n是链表的个数, 
     //k是每个链表的长度
     //不好: merge two by two,取两个排好, 再和第三个排, 以此类推, 复杂度是n^2k; (1+...+n)k + nk -> n^2k
@@ -28,24 +28,19 @@ public class Solution {
         {
             result.add(lists[i]);
         }*/
-    public ListNode mergeKLists(List<ListNode> lists) //lists里面装的都是链表头
-    {
-        if(lists == null || lists.size() == 0)
-        {
+    public ListNode mergeKLists(List<ListNode> lists) { //lists里面装的都是链表头
+        if(lists == null || lists.size() == 0) {
             return null;
         }
         
-        while(lists.size() > 1)
-        {
+        while(lists.size() > 1) {
             List<ListNode> newlists = new ArrayList<ListNode>();
-            for(int i = 0; i + 1 < lists.size(); i += 2)
-            {
+            for(int i = 0; i + 1 < lists.size(); i += 2) {
                 ListNode mergedlist = merge(lists.get(i), lists.get(i + 1)); //奇数个的时候就会不小于list.size()然后退出
                 newlists.add(mergedlist);
             }
             //有奇数个链表就把最后一个没有参与合并的加入到新的Arraylist里，在最后一次合并就会把它合并啦
-            if(lists.size() % 2 == 1) 
-            {
+            if(lists.size() % 2 == 1) {
                 newlists.add(lists.get(lists.size() - 1));
             }
             lists = newlists;
@@ -53,60 +48,48 @@ public class Solution {
         return lists.get(0); //最后合并成一个返回就行
     }
     
-    private ListNode merge(ListNode l1, ListNode l2) //如果一个为空，返回另一个链表；如果两个都为空，就返回空啦
-    {
-        if(l1 == null)
-        {
+    private ListNode merge(ListNode l1, ListNode l2) { //如果一个为空，返回另一个链表；如果两个都为空，就返回空啦
+        if(l1 == null) {
             return l2;
         }
-        if(l2 == null)
-        {
+        if(l2 == null) {
             return l1;
         }
         
         ListNode dummy = new ListNode(0);
         ListNode lastNode = dummy;
-        while(l1 != null && l2 != null)
-        {
-            if(l1.val < l2.val)
-            {
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
                 lastNode.next = l1;
                 l1 = l1.next;
             }
-            else
-            {
+            else {
                 lastNode.next = l2;
                 l2 = l2.next;
             }
             lastNode = lastNode.next; //lastNode是每次一定要窜的
         }
         
-        if(l1 != null) //说明l2为空, lastNode在l2上
-        {
+        if(l1 != null) { //说明l2为空, lastNode在l2上
             lastNode.next = l1; //把lastNode连到l1上
         }
-        else //说明l1为空, lastNode在l1上
-        {
+        else { //说明l1为空, lastNode在l1上
             lastNode.next = l2; //把lastNode连到l2上
         }
         return dummy.next;
     }
 
-//方法2：divide & conquer(自上而下),就是找中点，一半一半拆开分着做
+    //方法2：divide & conquer(自上而下),就是找中点，一半一半拆开分着做
     //复杂度还是nklog(k),就是方法1反着的求解
-    public ListNode mergeKLists(List<ListNode> lists)
-    {
-        if(lists == null || lists.size() == 0)
-        {
+    public ListNode mergeKLists(List<ListNode> lists) {
+        if(lists == null || lists.size() == 0) {
             return null;
         }
         return mergeHelper(lists, 0, lists.size() - 1);
     }
     
-    private ListNode mergeHelper(List<ListNode> lists, int start, int end)
-    {
-        if(start == end)
-        {
+    private ListNode mergeHelper(List<ListNode> lists, int start, int end) {
+        if(start == end) {
             return lists.get(start);
         }
         int mid = start + (end - start) / 2; //递归分解, 要merge所有list, 先merge中点左边, 再merge中点右边, 
@@ -118,49 +101,41 @@ public class Solution {
         return merge(left, right);
     }
     
-    private ListNode merge(ListNode l1, ListNode l2) //如果一个为空，返回另一个链表；如果两个都为空，就返回空啦
-    {
-        if(l1 == null)
-        {
+    private ListNode merge(ListNode l1, ListNode l2) { //如果一个为空，返回另一个链表；如果两个都为空，就返回空啦
+        if(l1 == null) {
             return l2;
         }
-        if(l2 == null)
-        {
+        if(l2 == null) {
             return l1;
         }
         
         ListNode dummy = new ListNode(0);
         ListNode lastNode = dummy;
-        while(l1 != null && l2 != null)
-        {
-            if(l1.val < l2.val)
-            {
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
                 lastNode.next = l1;
                 l1 = l1.next;
             }
-            else
-            {
+            else {
                 lastNode.next = l2;
                 l2 = l2.next;
             }
             lastNode = lastNode.next; //lastNode是每次一定要窜的
         }
         
-        if(l1 != null) //说明l2为空, lastNode在l2上
-        {
+        if(l1 != null) { //说明l2为空, lastNode在l2上
             lastNode.next = l1; //把lastNode连到l1上
         }
-        else //说明l1为空, lastNode在l1上
-        {
+        else { //说明l1为空, lastNode在l1上
             lastNode.next = l2; //把lastNode连到l2上
         }
         return dummy.next;
     }
     
-//方法3：Heap的方法:(用comparator),思路就是把每个链表的头结点装入优先级队列然后根据比较器自动挑出最小的，取出来，
-        //然后从取出点的那个链表再加入一个结点继续比较
-        //复杂度：knlog(n), n是lists的长度, k是每个链表的长度
-        //堆的长度是n, 所以每加入一个值就是一个log(n),总共加入nk次
+    //方法3：Heap的方法:(用comparator),思路就是把每个链表的头结点装入优先级队列然后根据比较器自动挑出最小的，取出来，
+    //然后从取出点的那个链表再加入一个结点继续比较
+    //复杂度：knlog(n), n是lists的长度, k是每个链表的长度
+    //堆的长度是n, 所以每加入一个值就是一个log(n),总共加入nk次
     //List里面装的都是每个链表的头结点
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists == null || lists.length == 0) {
@@ -200,7 +175,6 @@ public class Solution {
             return left.val - right.val; //comparator中左边减右边是从小到大排，右边减左边是从大到小排
         }
     }; //有个分号
-    
     
      //ListNode是自己写的类，要是比较类的话就得用comparator,指定比较类中的什么数值，进而按照这个数值进行排序，
      //如果不用comparator就会出现ClassCastException这个异常，叫类型转换错误，因为ListNode无法直接强转成comparator
