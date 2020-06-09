3d5745c8aa2594922c578288f4466089f6c88448
hesunnb <hesun63@gmail.com>
2020-06-09 08:46:40 -0400

    Update ArithmeticSlices.java

--- ArithmeticSlices.java
+++ ArithmeticSlices.java
@@ -35,7 +35,7 @@ class Solution {
         
         int curr = 0, sum = 0;
         for (int i = 2; i < A.length; i++) {
-            if (A[i]-A[i-1] == A[i-1]-A[i-2]) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
                 curr += 1; //动归解法, cur就是算上A[i]这个数的时候, 有多少个Arithmetic Slices
                 sum += curr; //sum存的是之前所有的Arithmetic Slices
             } else {
@@ -45,6 +45,27 @@ class Solution {
         return sum;
     }
     
+    //用数学规律运算
+    public int numberOfArithmeticSlices(int[] A) {
+        if(A == null || A.length < 3) {
+            return 0;
+        }
+        
+        int sum = 0;
+        int count = 2;
+        for(int i = 2; i < A.length; i++) {
+            while(i < A.length && A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                count++;
+                i++; //比如-1,1,3,3,3,2,3,2,1,0,4,10例子, 当i在4的时候已经判断为1,0,4不是连续相差1了, 那么上面的for循环就不用再从4开始了
+                //可以从4的下一位开始了, 也就是这里的i++和for循环的i++不冲突
+            }
+            if(count > 2) {
+                sum += (1 + count - 2) * (count - 2) / 2; //查出有多少个连续相等, 比如有5个连续相等, 那么就是5-2=3, 3+2+1就是结果
+                count = 2;
+            }
+        }
+        return sum;
+    }
     
     //solution2: (own), 时间O(n^2)
     public int numberOfArithmeticSlices(int[] A) {
